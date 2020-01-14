c6eb3035cfc67576fb52e6dddeb15732b86e970b
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-01-14 17:14:10 +0100

    Displaying tasks and rendering different icons and styles depending on task status.

--- code/src/components/TaskList.js
+++ code/src/components/TaskList.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { todoList } from 'reducers/todoList'
 import "./taskList.css"
 
 export const TaskList = () => {
+  const allTasks = useSelector(state => state.todoList.tasks)
+  console.log(allTasks)
   return (
     <ul className="task-list">
+      {allTasks.map(task => (
+        task.isCompleted ?
+          (<li className="task-item task-completed">
+            <button type="button" className="toggle-status-button">✔️
+            <span className="sr-only">Done</span>
+            </button>
+            {task.todoText}
+          </li>)
+          :
+          (<li className="task-item task-todo">
+            <button type="button" className="toggle-status-button">❌
+            <span className="sr-only">Todo</span>
+            </button>
+            {task.todoText}
+          </li>)
+      ))}
       <li className="task-item task-completed"><button type="button" className="toggle-status-button">✔️<span className="sr-only">Done</span></button> This is a task</li>
       <li className="task-item task-completed"><button type="button" className="toggle-status-button">✔️<span className="sr-only">Done</span></button>  This is another task</li>
       <li className="task-item task-todo"><button type="button" className="toggle-status-button">❌<span className="sr-only">Todo</span></button>  This is a third task</li>
-    </ul>
+    </ul >
   )
 }
