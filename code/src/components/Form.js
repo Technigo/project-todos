1c58082a96b0bd86a73b3e4271163c93eeffa7d0
Karol Cislowski <cislowski.karol@hotmail.com>
2020-01-14 22:46:04 +0100

    Form Component

---
+++ code/src/components/Form.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { quests } from 'reducer/quests'
+
+export const Form = () => {
+  const dispatch = useDispatch()
+
+  const categoriesList = useSelector((state) => state.quests.categories)
+  const [text, setText] = useState()
+  const [category, setCategory] = useState(categoriesList[0])
+
+  const handleChangeText = (e) => { setText(e.target.value) }
+  const handleChangeCategory = (e) => setCategory(e.target.value)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(quests.actions.createQuest({ text, category }))
+    setText('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={text} onChange={handleChangeText} />
+      <select
+        onChange={handleChangeCategory}
+        value={category}>
+        {categoriesList.map((e, index) => (
+          <option value={e} key={`option-${index}`}>{e}</option>
+        ))}
+      </select>
+      <button type="submit">Add</button>
+    </form>
+  )
+}
--- code/src/components/QuestsLog.js
+++ code/src/components/QuestsLog.js
@@ -1,42 +1,17 @@
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { quests } from 'reducer/quests'
+import React from 'react'
+import { useSelector } from 'react-redux'
 
 import { Quest } from 'components/Quest'
+import { Form } from 'components/Form'
 
 export const QuestsLog = () => {
-  const dispatch = useDispatch()
-  const categoriesList = useSelector((state) => state.quests.categories)
   const questsList = useSelector((state) => state.quests.questsList)
-
-  const [text, setText] = useState()
-  const [category, setCategory] = useState(categoriesList[0])
-
-  const handleChangeText = (e) => { setText(e.target.value) }
-  const handleChangeCategory = (e) => setCategory(e.target.value)
-  const handleClick = () => {
-    dispatch(quests.actions.createQuest({ text, category }))
-    setText('')
-  }
-
-  const options = categoriesList.map((e, index) => {
-    const key = `option-${index}`
-
-    return (
-      <option value={e} key={key}>{e}</option>
-    )
-  })
 
   return (
     <>
-      {questsList.map((e, index) => (<Quest key={`quest-${index}`} questIndex={index} />))}
-      <input type="text" value={text} onChange={handleChangeText} />
-      <select
-        onChange={handleChangeCategory}
-        value={category}>
-        {options}
-      </select>
-      <button type="button" onClick={handleClick}>Add</button>
+      {questsList.map((e, index) => (
+        <Quest key={`quest-${index}`} questIndex={index} />))}
+      <Form />
     </>
   )
 }
