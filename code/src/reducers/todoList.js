fc279f6b61ac3ff417175497bc6c07894e7f2ccd
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-01-15 18:03:31 +0100

    Refactored toggleTaskStatus

--- code/src/reducers/todoList.js
+++ code/src/reducers/todoList.js
@@ -7,17 +7,14 @@ export const todoList = createSlice({
   },
   reducers: {
     addTask: (state, action) => {
-      const taskId = 1 + state.tasks.length;
       state.tasks.push({ id: Date.now(), todoText: action.payload, isCompleted: false })
     },
     toggleTaskStatus: (state, action) => {
       const existingTask = state.tasks.find((task) => task.id === action.payload)
 
-      if (existingTask && existingTask.isCompleted === true) {
-        existingTask.isCompleted = false;
-      } else if (existingTask && existingTask.isCompleted === false) {
-        existingTask.isCompleted = true;
-      }
+      // If the task is already completed set isCompleted to false, otherwise to true to toggle task status
+      existingTask.isCompleted ? existingTask.isCompleted = false : existingTask.isCompleted = true
+
     }
 
   }
