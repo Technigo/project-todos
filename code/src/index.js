97572a544ebb23cbb5e6aed5678e865a189b9143
ucha3i <jchabik@gmail.com>
2020-01-15 14:32:32 +0100

    code along with redux example from their website

--- code/src/index.js
+++ code/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { App } from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
+const store = createStore(rootReducer)
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
