d71a4a942f86faccd89349f3d4d9a5dc1fc908f4
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-01-16 16:38:07 +0100

    Changed name of variable for tasks total and added logic to display task or tasks depending on tasks total number.

--- code/src/components/TodoCount.js
+++ code/src/components/TodoCount.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { todoList } from 'reducers/todoList'
+import "./todoCount.css"
 
 export const TodoCount = () => {
   const unfinishedTasks = useSelector(state => state.todoList.tasks.filter(task => !task.isCompleted))
-  const numberOfUnfinishedTasks = unfinishedTasks.length;
+  const tasksTotal = unfinishedTasks.length;
   return (
-    <p>{numberOfUnfinishedTasks}</p>
+    <p className="todo-count">{tasksTotal} {tasksTotal === 1 ? "task" : "tasks"}</p>
   )
 }
