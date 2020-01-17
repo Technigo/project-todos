291df4b12e5e466956b5ad9a9962b8e72fff720b
horsefacegirl <51134090+horsefacegirl@users.noreply.github.com>
2020-01-17 13:11:39 +0100

    initial commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { AddTodo } from './components/AddTodo'
+import { TodoList } from './components/TodoList'
 
-export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+const reducer = combineReducers({})
+const store = configureStore({ reducer })
+
+export const App = () => (
+  <Provider store={store}>
+    <AddTodo />
+    <TodoList />
+  </Provider>
+)
---
+++ code/src/components/TodoList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { TodoItem } from './TodoItem'
+
+export const TodoList = () => {
+  return (
+    <>
+    </>
+  )
+}
