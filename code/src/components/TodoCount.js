ef73b3c7702fddf35527e3a4d06055965edfdbe0
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-01-19 12:57:22 +0100

    Fixed formatting

--- code/src/components/TodoCount.js
+++ code/src/components/TodoCount.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react"
+import { useSelector } from "react-redux"
+
 import "./todoCount.css"
 
 export const TodoCount = () => {
   const unfinishedTasks = useSelector(state => state.todos.tasks.filter(task => !task.isCompleted))
-  const tasksTotal = unfinishedTasks.length;
+  const tasksTotal = unfinishedTasks.length
+
   return (
     <p className="todo-count">{tasksTotal} {tasksTotal === 1 ? "task" : "tasks"}</p>
   )
