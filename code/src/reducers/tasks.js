db907c6ceb112a7d264c1398527a2a1f1cf5be7f
nasimmhn <nasim.mahzoun@gmail.com>
2020-01-19 22:20:24 +0100

    more cleaning code

--- code/src/reducers/tasks.js
+++ code/src/reducers/tasks.js
@@ -2,11 +2,11 @@ import { createSlice } from '@reduxjs/toolkit'
 
 
 const taskData = [
-  { id: 1, text: 'Watch video on actions & reducers', complete: true },
-  { id: 2, text: 'Follow redux codealong', complete: false },
-  { id: 3, text: 'Fork weekly assignment', complete: true },
-  { id: 4, text: 'Update LinkedIn profile ', complete: true },
-  { id: 5, text: 'Create a todo app', complete: false },
+  // { id: 1, text: 'Watch video on actions & reducers', complete: true },
+  // { id: 2, text: 'Follow redux codealong', complete: false },
+  // { id: 3, text: 'Fork weekly assignment', complete: true },
+  // { id: 4, text: 'Update LinkedIn profile ', complete: true },
+  // { id: 5, text: 'Create a todo app', complete: false },
 ]
 
 export const tasks = createSlice({
