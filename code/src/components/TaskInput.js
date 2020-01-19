0b0be27a7b60c9e40846f17a9d08b2fdd5074f94
nyblad <nyblad@hotmail.com>
2020-01-19 12:58:47 +0100

    Maxlength 20 chars

--- code/src/components/TaskInput.js
+++ code/src/components/TaskInput.js
@@ -20,7 +20,7 @@ export const TaskInput = () => {
         type='text'
         required
         placeholder='Add task'
-        maxLength='60'
+        maxLength='20'
         onChange={(event) => setNewTask(event.target.value)}
         value={newTask}
         // To add the task when hitting enter key
