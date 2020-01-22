6dcd9df022df88f3c69311f0698142bf87feddde
nasimmhn <nasim.mahzoun@gmail.com>
2020-01-22 10:50:55 +0100

    styled the dialog's buttons

--- code/src/components/Dialog.js
+++ code/src/components/Dialog.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 
 import { tasks } from 'reducers/tasks'
@@ -11,7 +11,7 @@ const Container = styled.div`
   position: absolute;
   left: calc(50% - 165px);
   top: 250px;
-  width:300px;
+  width:320px;
   padding:12px;
   align-self:center;
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0,0,0,.12);
@@ -24,29 +24,35 @@ const Title = styled.h2`
   margin:0;
 `
 
+
 const Supportingtext = styled.p`
   margin: 0;
   color: #6b6b6b;
+  font-size:18px;
   `
+
 
 const DialogActions = styled.div`
   padding:8px;
   display:flex;
-   
+  justify-content:flex-end;
 `
 
 const DialogButton = styled.div`
-  padding:10px;
-
+  padding:10px; 
 `
+
 const ButtonText = styled.button`
-  padding:5px;
+  padding:12px;
+  font-size:18px;
+  background-color:transparent;
+  border-style:none;
 `
 
 
 
 
-export const Dialog = ({ title, supportingText, primaryBtnText, secondaryBtnText, showDialog }) => {
+export const Dialog = ({ title, supportingText, primaryBtnText, secondaryBtnText, showDialog, test }) => {
   const dispatch = useDispatch()
 
   const btnClicked = (btnText) => {
@@ -69,17 +75,16 @@ export const Dialog = ({ title, supportingText, primaryBtnText, secondaryBtnText
     <>
       {showDialog &&
         <Container>
-
           <div>
             {title && <Title>{title}</Title>}
             {supportingText && <Supportingtext>{supportingText}</Supportingtext>}
           </div>
           <DialogActions>
-            <DialogButton onClick={() => { }}>
-              {primaryBtnText && <ButtonText role="button" aria-label={primaryBtnText} onClick={() => btnClicked(primaryBtnText)}>{primaryBtnText}</ButtonText>}
+            <DialogButton className={test} onClick={() => { }}>
+              {secondaryBtnText && <ButtonText role="button" aria-label={secondaryBtnText} onClick={() => btnClicked(secondaryBtnText)}>{secondaryBtnText}</ButtonText>}
             </DialogButton>
             <DialogButton onClick={() => { }}>
-              {secondaryBtnText && <ButtonText role="button" aria-label={secondaryBtnText} onClick={() => btnClicked(secondaryBtnText)}>{secondaryBtnText}</ButtonText>}
+              {primaryBtnText && <ButtonText role="button" aria-label={primaryBtnText} onClick={() => btnClicked(primaryBtnText)}> {primaryBtnText}</ButtonText>}
             </DialogButton>
           </DialogActions>
         </Container>
