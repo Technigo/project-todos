6a8215a6a816abcd1d9170397789ede6cd95dbc7
CissiWong <cissi.wong@gmail.com>
2020-03-31 21:46:48 +0200

    added count to active and completed

--- code/src/components/Header/TaskSummary.js
+++ code/src/components/Header/TaskSummary.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-
 const TaskSummary = () => {
+
   const tasks = useSelector(state => state.tasks).length
-  return (
-    <div>{tasks}</div>
+  const completed = useSelector((state) => state.tasks.filter((item) => item.completed)).length
+  const active = useSelector((state) => state.tasks.filter((item) => !item.completed)).length
+
+  return(
+    <div>
+      <div>{tasks}</div>
+      <div>{completed}</div>
+      <div>{active}</div>
+    </div>
+    
   )
 }
 
