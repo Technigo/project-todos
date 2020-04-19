9e1799d1898b25b9a3826cd106b2ad31c025eee6
Frida Eriksson <Frida.Eriksson@silf.se>
2020-04-19 15:54:26 +0200

    added persistence to Home, persisting the state in localstorage

--- code/src/pages/Home.js
+++ code/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from "react-redux"
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { createStore, combineReducers } from '@reduxjs/toolkit'
 import { todos } from '../reducers/todos'
 import { TodoList } from '../components/TodoList'
 
@@ -8,11 +8,17 @@ import { TodoList } from '../components/TodoList'
 const reducer = combineReducers({ todos: todos.reducer })
 
 // Persistence: Retrieve the existing state from localstorage if it exists
+const persistedState = localStorage.getItem("reduxState")
+  ? JSON.parse(localStorage.getItem("reduxState"))
+  : {};
 
 // StoreCreation: Create the store using our reducers and the retrieved state
-const store = configureStore({ reducer })
+const store = createStore(reducer, persistedState)
 
 // Persistence: Tell the store to persist the state in localstorage after every action
+store.subscribe(() => {
+  localStorage.setItem("reduxState", JSON.stringify(store.getState()));
+});
 
 export const Home = () => {
   return (
