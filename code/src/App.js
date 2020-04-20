60efe61370a35319a4119154ef1055aea0a25daa
Lisa Hammarstrand <lisa@Lisas-Air.lan>
2020-04-20 20:17:00 +0200

    Initial commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { tasks } from './reducers/tasks'
+import { TaskList } from './components/TaskList'
+
+const reducer = combineReducers({
+  tasks: tasks.reducer
+})
+
+const store = configureStore({ reducer })
+
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}>
+      <TaskList></TaskList>
+    </Provider>
   )
 }
