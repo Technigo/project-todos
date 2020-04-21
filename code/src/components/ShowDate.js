f1ed059a6feba4cd14ecdb737868f68fb12227b5
Ingrid Hammargren <ingrid.hammargren@gmail.com>
2020-04-21 10:47:37 +0200

    added todays date

---
+++ code/src/components/ShowDate.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Moment from 'react-moment'
+import 'moment-timezone'
+
+export const ShowDate = () => {
+  const date = new Date()
+
+  return (
+
+    <section >
+      <Moment format="YYYY-MM-DD">{date}</Moment>
+    </section>
+
+
+  )
+}
--- code/src/pages/Home.js
+++ code/src/pages/Home.js
@@ -3,6 +3,7 @@ import { TodoInput } from 'components/TodoInput'
 import { TodoList } from 'components/TodoList'
 import { TodoSummery } from 'components/TodoSummery'
 import { todos } from 'reducers/todos'
+import { ShowDate } from 'components/ShowDate'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
@@ -14,6 +15,7 @@ export const Home = () => {
   return (
     <Provider store={store}>
       <main>
+        <ShowDate />
         <TodoList />
         <TodoSummery />
       </main>
