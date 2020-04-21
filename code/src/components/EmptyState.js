c07af63e0866a1c9a5085560f269f379f9ab751f
Frida Eriksson <Frida.Eriksson@silf.se>
2020-04-21 19:13:23 +0200

    updated structure and styling of EmptyState

--- code/src/components/EmptyState.js
+++ code/src/components/EmptyState.js
@@ -1,16 +1,6 @@
 import React from 'react'
-import checklist from '../assets/checklist.jpg'
 import styled from 'styled-components/macro'
-
-export const EmptyState = () => {
-  return (
-    <EmptyContainer>
-      <Image src={checklist} alt="checklist with 4 people around it" />
-      <Text>What do you want to do today?</Text>
-      <Text>Start adding tasks to your todo list.</Text>
-    </EmptyContainer>
-  )
-}
+import checklist from '../assets/checklist.jpg'
 
 const EmptyContainer = styled.section`
   display: flex;
@@ -21,19 +11,18 @@ const EmptyContainer = styled.section`
 `
 
 const Image = styled.img`
-  width: 100%;
+  width: 70%;
   height: auto;
-  margin: 10px 0 20px 0;
+  margin: 50px 0 20px 0;
   border-radius: 10px;
 
   @media (min-width: 768px) {
-    width: 70%;
     margin-top: 100px;
   }
 
   @media (min-width: 1024px) {
     width: 30%;
-    margin-top: 20px;
+    margin-top: 50px;
   }
 `
 
@@ -46,3 +35,17 @@ const Text = styled.p`
     font-size: 1em;
   }
 `
+
+export const EmptyState = () => {
+  return (
+    <EmptyContainer>
+
+      <Image src={checklist} alt="Illustrated checklist with people around it" />
+
+      <Text>What do you want to do today?</Text>
+
+      <Text>Start adding tasks to your todo list.</Text>
+
+    </EmptyContainer>
+  )
+}
