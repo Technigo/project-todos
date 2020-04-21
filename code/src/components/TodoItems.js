e242bcc02490c00dbe61fa76064f1ce89d6ab0dd
Frida Eriksson <Frida.Eriksson@silf.se>
2020-04-21 19:08:57 +0200

    updated structure of TodoItems

--- code/src/components/TodoItems.js
+++ code/src/components/TodoItems.js
@@ -1,14 +1,37 @@
 import React from 'react'
-import { TodoItem } from './TodoItem'
 import styled from 'styled-components/macro'
 import { useSelector } from 'react-redux'
+import { TodoItem } from './TodoItem'
 import { EmptyState } from './EmptyState'
+
+const TodoListing = styled.section`
+  flex: 1;
+  width: 90%;
+  margin: 5px auto;
+  overflow: auto;
+  
+  ::-webkit-scrollbar {
+    width: 4px;
+    padding-right: -4px;
+  }
+ 
+  ::-webkit-scrollbar-track {
+      background: #eaceca;
+      border-radius: 10px;
+  }
+ 
+  ::-webkit-scrollbar-thumb {
+      background: #b55136;
+      border-radius: 10px; 
+  }
+`
 
 export const TodoItems = () => {
   const items = useSelector(store => store.todos.items)
 
   return (
     <TodoListing>
+
       {items.length > 0 &&
         <>
           {items.map((item, index) => (
@@ -20,28 +43,7 @@ export const TodoItems = () => {
       {items.length === 0 &&
         <EmptyState />
       }
+
     </TodoListing>
   )
 }
-
-const TodoListing = styled.section`
-  flex: 1;
-  width: 90%;
-  margin: 5px auto;
-  overflow: auto;
-  
-  ::-webkit-scrollbar {
-    width: 4px;
-    padding-right: -4px;
-}
- 
-::-webkit-scrollbar-track {
-    background: #eaceca;
-    border-radius: 10px;
-}
- 
-::-webkit-scrollbar-thumb {
-    background: #b55136;
-    border-radius: 10px; 
-}
-`
