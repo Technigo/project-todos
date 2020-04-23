8433d9907fff307bba4619cc376082c1b0116309
Emmie <emelie_loheden@hotmail.com>
2020-04-23 13:32:36 +0200

    clear all if there is task in sumary

--- code/src/components/TodoSummary.js
+++ code/src/components/TodoSummary.js
@@ -15,12 +15,11 @@ const handleClick = () => {
 }
 
   return (
-  <SummaryContainer >
-      <h2>
+    <SummaryContainer >
+      <Form> {list.items.length <= 0 ? <h3>Add something to do!</h3> : <h2>
         {numDone}/{list.items.length} done
-      </h2>
-      <ClearButton onClick={handleClick}>Clear all</ClearButton>
-      <Form> {list.items.length <= 0 ? <h3>Add something to do!</h3> : ''}</Form>
-  </SummaryContainer>
+        <ClearButton onClick={handleClick}>Clear all</ClearButton>
+        </h2>}</Form>
+    </SummaryContainer>
   )
-};
+}
