f6788e0e70dd6d47cf6ba48bfc1bbd1680f8049b
Mathias Thorström <thisen@gmail.com>
2020-04-23 19:26:10 +0200

    fixed filter component to work

--- code/src/components/ShowFilter.js
+++ code/src/components/ShowFilter.js
@@ -5,10 +5,10 @@ import { todo } from "../reducers/todo"
 
 const FilterSection = styled.section`
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
   background-color: DeepPink;
-  width: 10vw;
+  width: 12vw;
   height: 5vw;
   border-bottom-right-radius: 10px;
   border-bottom-left-radius: 10px; 
@@ -18,8 +18,20 @@ const FilterSection = styled.section`
   margin-bottom: 10px;
 `
 const A = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  background-color: ${props => props.default ? 'white' : props.work ? '#FFBB00' : props.personal ? '#7CBB00' : props.other ? '#00A1F1' : 'white'};
+  border: 1px solid black;
   cursor: pointer;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  color: black;
 `
+
+
 export const ShowFilter = () => {
   const categorys = useSelector((store) => store.todo.todoListItems)
 
@@ -36,16 +48,15 @@ export const ShowFilter = () => {
 
   return (
     <FilterSection>
-      <button onClick={(event) => setFilter(event.target.value)} value='work'>
-        Work
-      </button>
-      <button onClick={(event) => setFilter(event.target.value)} value='personal'>
-        personal
-      </button>
+      <A default onClick={() => dispatch(todo.actions.filterTodo({filter: '',}))}>A</A>
+      <A work onClick={() => dispatch(todo.actions.filterTodo({filter: 'work',}))}>W</A>
+      <A personal onClick={() => dispatch(todo.actions.filterTodo({filter: 'personal',}))}>P</A>
+      <A other onClick={() => dispatch(todo.actions.filterTodo({filter: 'other',}))}>O</A>
       <span>{filter}</span>
     </FilterSection>
   )
 }
+
 
 {/* <form handle dispatch>
   <button update filter value></button>
