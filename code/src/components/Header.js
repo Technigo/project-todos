e23e2e33baeb1869898c4801cddf57df1fe2496c
Ruterryter <hanna.ruter@gmail.com>
2020-04-23 18:55:53 +0200

    fixed intendation css file

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -12,23 +12,23 @@ export const Header = () => {
 }
 
 const StyledHeader = styled.header`
- background-color:#1e3756;
- border-radius: 100% 0% 100% 0% / 0% 79% 55% 100% ;
- height: 170px;
+  background-color:#1e3756;
+  border-radius: 100% 0% 100% 0% / 0% 79% 55% 100% ;
+  height: 170px;
 `
 const StyledText = styled.text`
-color: #FFFF;
-display: flex;
-flex-direction: column;
-padding: 40px;
-font-size: 48px;
-font-weight: 700;
-font-family: 'niveau-grotesk', sans-serif ;
-font-style: italic;
+  color: #FFFF;
+  display: flex;
+  flex-direction: column;
+  padding: 40px;
+  font-size: 48px;
+  font-weight: 700;
+  font-family: 'niveau-grotesk', sans-serif ;
+  font-style: italic;
 `
 const StyledBackground = styled.div`
-background-color:#f18666;
-height: 50px;
-width: 100%;
-border-bottom-right-radius: 55% 120%;
+  background-color:#f18666;
+  height: 50px;
+  width: 100%;
+  border-bottom-right-radius: 55% 120%;
 `
--- code/src/components/TodoItem.js
+++ code/src/components/TodoItem.js
@@ -51,4 +51,3 @@ export const TodoItem = ({ itemIndex }) => {
     </div>
   );
 };
-
