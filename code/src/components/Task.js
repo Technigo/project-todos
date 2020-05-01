3a07e13f25ee65d713296d6ad809fdf9b1ae6ca1
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-05-01 03:08:31 +0200

    last push for today

--- code/src/components/Task.js
+++ code/src/components/Task.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { todo } from '../reducers/todo'
 
 export const Task = (props) => {
-  const { task, category, completed } = props.item
+  const { id, task, category, completed, dueDate, startDate } = props.item
+
+  const dispatch = useDispatch()
+
+  const handleCheckboxClick = () => {
+    dispatch(todo.actions.toggleCheckbox(id))
+  }
+
+  const handleRemoveButton = () => {
+    dispatch(todo.actions.removeItem(id))
+  }
 
   return (
     <>
       <li>
         <span>{category}: </span>
         <span>{task}</span>
+        <span>{startDate}</span>
+        {/* <span>{dueDate}</span> */}
         <input
           type="checkbox"
           isChecked={completed}
-          onChangeHandler={() => { }}
+          onChangeHandler={handleCheckboxClick}
         />
-        <button onClick={() => { }}>remove</button>
+        <button onClick={handleRemoveButton}>remove</button>
       </li>
     </>
   )
--- code/src/reducers/todo.js
+++ code/src/reducers/todo.js
@@ -9,5 +9,30 @@ export const todo = createSlice({
       { id: 3, task: 'Assignment', completed: false, category: 'Work' }
     ]
   },
-  reducers: {}
+  reducers: {
+    addItem: (state, action) => {
+      const { task, category, dueDate } = action.payload
+      state.items.push({
+        id: Date.now(),
+        task,
+        completed: false,
+        category,
+        startDate: Date.now(),
+        dueDate,
+      })
+    },
+    removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.id !== action.payload)
+    },
+    removeAll: (state, action) => {
+      state.items = []
+    },
+    toggleCheckbox: (state, action) => {
+      const foundItem = state.items.find((item) => item.id === action.payload)
+
+      if (foundItem) {
+        foundItem.completed = !foundItem.completed
+      }
+    },
+  },
 })
