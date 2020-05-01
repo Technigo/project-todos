ac62448735ec5164fcc8f8302a3b714c5ea280ae
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-05-01 02:00:34 +0200

    added due date

--- code/src/components/AddTask.js
+++ code/src/components/AddTask.js
@@ -1,21 +1,25 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { todo } from '../reducers/todo'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 export const AddTask = () => {
   const [task, setTask] = useState('')
   const [category, setCategory] = useState('')
+  const [dueDate, setDueDate] = useState()
 
   const dispatch = useDispatch()
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    dispatch(todo.actions.addItem({ task, category })) // name, category as payload
-    setTask('') // Clearing the input
-    setCategory('') // Clearing the input
+    dispatch(todo.actions.addItem({ task, category, dueDate })) // name, category, duedate as payload
+    setTask('')
+    setCategory('')
+    setDueDate('')
   }
 
-  const enabled = task.length > 0
+  const enabled = task.length > 0 && dueDate > 0 && category.length > 0
 
   return (
     <>
@@ -24,10 +28,11 @@ export const AddTask = () => {
         <label>
           Category:
           <select
-            value={{}}
-            onChange={() => { }}
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
           >
-            <option value='To-Do'>To-Do</option>
+            <option value='' selected>select...</option>
+            <option value='To-Do' selected>To-Do</option>
             <option value='Shopping'>Shopping</option>
             <option value='Work'>Work</option>
             <option value='Family'>Family</option>
@@ -40,10 +45,14 @@ export const AddTask = () => {
           <input
             type='text'
             value={task}
-            required
             placeholder='+ add a new task'
             onChange={(event) => setTask(event.target.value)}
           />
+        </label>
+
+        <label>
+          Due date:
+          <DatePicker onChange={(date) => setDueDate(date)} selected={dueDate} />
         </label>
 
         <button type='submit' disabled={!enabled}>Add task</button>
