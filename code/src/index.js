484d62c3311193bec91c47eac1a174876e4eeff2
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-20 17:36:24 +0100

    Initial commit: Setting up package.json

--- code/src/index.js
+++ code/src/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { App } from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { App } from './App';
+
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
