9acba8c0405e018c67e28634df07dc89fb5685d0
Rebkova <rebkova@gmail.com>
2020-11-21 18:08:45 +0100

    store created

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Provider } from "react-redux"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import { todoTasks } from 'reducer/todoTasks'
+
+import todoTasks from "./reducer/todoTasks"
+
+//creating a single reducer from several reducers
+export const reducer = combineReducers({
+  todoTasks: todoTasks.reducer
+})
+
+//using the reducer to create a store
+const store = configureStore({ reducer: reducer }) //can also be written as {reducer}
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    //store is passed to provider as a prop
+    <Provider store={store}>
+      <div>
+        Find me in src/app.js!
+      </div>
+    </Provider>
   )
 }
--- code/src/reducer/todoTasks.js
+++ code/src/reducer/todoTasks.js
@@ -1,11 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+const allTodoTasks = [
+  { id: 1, text: 'Watch video on actions & reducers', complete: true },
+  { id: 2, text: 'Follow redux codealong', complete: true },
+  { id: 3, text: 'Fork weekly assignment', complete: true },
+  { id: 4, text: 'Create a todo app', complete: false },
+]
+
 export const todoTasks = createSlice({
   name: "todoTasks",
-  initialState: [
-    { id: 1, text: 'Watch video on actions & reducers', complete: true },
-    { id: 2, text: 'Follow redux codealong', complete: true },
-    { id: 3, text: 'Fork weekly assignment', complete: true },
-    { id: 4, text: 'Create a todo app', complete: false },
-  ]
+  initialState: allTodoTasks
 }) 
