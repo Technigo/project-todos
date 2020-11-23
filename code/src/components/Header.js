5d2e4901dcbd56b1343986e47701bbcdfb1c41cf
Sara Sundblom <sundblomsara@gmail.com>
2020-11-23 11:09:34 +0100

    set up come reducers and a task list

---
+++ code/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Datepicker from '@reduxjs/toolkit'
+
+export const Header = () => {
+  return (
+  <h1>TO-DO</h1>
+  )
+}
+
+// export const Date = () => {
+//   const [startDate, setStartDate] = useState(new Date());
+//   return (
+//     <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+//   );
+// };
