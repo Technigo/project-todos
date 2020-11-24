4eccedc0d0e8b2662ec83cf9d06693b2ee54ff45
Olof Lindahl <olindahl@gmail.com>
2020-11-24 13:35:17 +0100

    set date, action button, filters working

--- code/src/App.js
+++ code/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { Container, Paper, Box, Fab, Dialog } from "@material-ui/core";
+import { Container, Paper, Box, Fab, Dialog, Tooltip } from "@material-ui/core";
 import styled from "styled-components";
-import { createMuiTheme, ThemeProvider, useTheme } from '@material-ui/core/styles';
+import {
+  createMuiTheme,
+  ThemeProvider,
+  useTheme,
+} from "@material-ui/core/styles";
 
 import { tasks } from "reducers/tasks";
 import { TodoList } from "components/TodoList";
 import { TodoInput } from "components/TodoInput";
-import { Header } from "components/Header"
-import theme from './theme';
+import { Header } from "components/Header";
+import theme from "./theme";
 
 //not really needed for one, but add more here if you want to combine reducers
 const reducer = combineReducers({ tasks: tasks.reducer });
@@ -18,26 +22,26 @@ const store = configureStore({ reducer });
 
 //special syntax needed to override styling of MUI (&&)
 const MyContainer = styled(Container)`
-   && {
-   padding: 0;
-   }`;
+  && {
+    padding: 0;
+  }
+`;
 
 const MyFab = styled(Fab)`
-&& {
-  position: fixed;
-  bottom: 20px;
-  right: 30px;
-}
-`
+  && {
+    position: fixed;
+    bottom: 20px;
+    right: 30px;
+  }
+`;
 
-  // const theme = createMuiTheme({
-  //   palette: {
-  //     type: 'dark',
-  //   }
-  // })
+// const theme = createMuiTheme({
+//   palette: {
+//     type: 'dark',
+//   }
+// })
 
 export const App = () => {
-
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -45,20 +49,30 @@ export const App = () => {
   };
 
   const handleClose = () => {
-    setOpen(false);}
-  
+    setOpen(false);
+  };
+
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-      <MyContainer maxWidth="xs">
-        <Header/>
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <TodoInput onClose={handleClose}/>
-        </Dialog>
-        <TodoList />
-        <MyFab color="primary" aria-label="add" onClick={handleClickOpen}>       
-      </MyFab>
-      </MyContainer>
+        <MyContainer maxWidth="xs">
+          <Header />
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="form-dialog-title"
+          >
+            <TodoInput onClose={handleClose} />
+          </Dialog>
+          <TodoList />
+          <Tooltip title='Add Todo'>
+          <MyFab
+            color="primary"
+            aria-label="add"
+            onClick={handleClickOpen}
+          ></MyFab>
+          </Tooltip>
+        </MyContainer>
       </ThemeProvider>
     </Provider>
   );
