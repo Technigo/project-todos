ca17efdd7952eebc6e353017161bd618e2238419
Therese <64402455+Thessan@users.noreply.github.com>
2020-11-24 14:35:16 +0100

    first commit, header

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { Header } from './components/Header'
+
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      <Header />
+    </>
   )
 }
---
+++ code/src/components/Header.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import headerImage from '../header.jpg'
+
+export const Header = () => {
+
+    const HeaderContainer = styled.section `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    width: 100%;
+    height: auto;
+    `
+
+    const Image = styled.img`
+    width: 100%;
+    height: auto;
+    `
+
+    const HeaderText = styled.text `
+    color: #000;
+    font-size: 22px;
+    font-style: italic;
+    position: absolute;
+    top: 50;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    padding-left: 10px;
+    padding-bottom: 2px;
+        @media (min-width: 768px) { /* TABLET VIEW */
+            font-size: 45px;
+            padding-left: 20px;
+        }
+            @media (min-width: 1024px) { /* DESKTOP VIEW */
+                font-size: 60px;
+                padding-left: 20px;
+            }
+    `
+
+
+    return (
+        <HeaderContainer>
+        <Image src={headerImage} />
+        <HeaderText>What to do?</HeaderText>
+        </HeaderContainer>
+    )
+
+}
