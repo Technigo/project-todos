334d53bbc5df16c1aa6437ccb148ddd97d99f53a
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-11-25 14:20:17 +0100

    Reducers and Basic Structure Created + Logic Added

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+
+import { TodoScreen } from './components/TodoScreen'
+import { tasklist } from './reducers/tasklist'
+
+const reducer = combineReducers({
+  tasklist = tasklist.reducer
+})
+
+const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}>
+      <TodoScreen></TodoScreen>
+    </Provider>
   )
 }
