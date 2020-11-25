44e17f095c74ed38229eb631cf53af8f83ba3322
Katsiez <katarinajovanovic.a@gmail.com>
2020-11-25 17:33:23 +0100

    kinda done with the reducer finally

--- code/src/reducers/tasks.js
+++ code/src/reducers/tasks.js
@@ -3,13 +3,13 @@ import moment from "moment"
 const initialState = {
 	
 	items: [
-		{id: 1, text: 'Read a book', category: 'Self Care', complete: true,},
-		{id: 2, text: 'Take the dog out', category: 'Household', complete: true,},
-		{id: 3, text: 'Do laundry', category: 'Household', complete: true,},
-		{id: 4, text: 'Make dinner', category: 'Household', complete: false,},
-		{id: 5, text: 'Do some more coding', category:'Coding', complete: false,},
-		{id: 6, text: 'Buy carrots', category:'Groceries', complete: false,},
-		{id: 7, text: 'Wash hair', category:'Other', complete: false,}
+		{id: 1, name: 'Read a book', category: 'Self Care', complete: true, checked: true},
+		{id: 2, name: 'Take the dog out', category: 'Household', complete: true, checked: false},
+		{id: 3, name: 'Do laundry', category: 'Household', complete: true, checked: true},
+		{id: 4, name: 'Make dinner', category: 'Household', complete: false, checked: true},
+		{id: 5, name: 'Do some more coding', category:'Coding', complete: false, checked: false},
+		{id: 6, name: 'Buy carrots', category:'Groceries', complete: false, checked: true},
+		{id: 7, name: 'Wash hair', category:'Other', complete: false, checked: false}
 
 ]}
 
@@ -27,7 +27,13 @@ export const tasks = createSlice({
 		removeAllTasks: (state) => {
 			state.items=[]
 		},
-		
+		checkTodoTask: (state,action) => {
+			const checkedTask = state.items.find((item) => item.id===action.payload)
+
+			if(checkedTask){
+				checkedTask.checked = !checkedTask.checked
+			}
+		}
 	}
 	
 })
