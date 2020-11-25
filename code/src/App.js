0866a3264e6bc81c0e04ef2062e31744a33ea357
idsintehittapa <jblom15@gmail.com>
2020-11-25 15:11:48 +0100

    local storage added

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore, createStore } from '@reduxjs/toolkit'
 
 import { Provider } from 'react-redux'
 
@@ -16,7 +16,29 @@ const reducer = combineReducers({
 })
 
 // a store with the reducer(s)
-const store = configureStore({ reducer })
+// const store  = configureStore({ reducer })
+
+// local storage
+// 1. retrieve local storage and use it as initial state
+
+const persistentStateJSON = localStorage.getItem('reduxState')
+console.log(`persistentStateJSON: ${persistentStateJSON}`)
+let persistentState = {}
+
+if (persistentStateJSON) {
+  persistentState = JSON.parse(persistentStateJSON)
+}
+console.log(`persistentState: ${persistentState}`)
+
+// // 2. Create the store using the initial state
+const store = createStore(reducer, persistentState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
+
+// // 3. Store the state in local storage at ANY redux state change
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
+
+
 
 export const App = () => {
   return (
