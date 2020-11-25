a0324f93f0240c3586a72311b74f927c92a2ac2d
Sara Sundblom <sundblomsara@gmail.com>
2020-11-25 17:36:54 +0100

    started styling of task

--- code/src/components/Task.js
+++ code/src/components/Task.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import styled from 'styled-components'
 
 import { tasks } from 'reducers/tasks'
+
+const TaskContainer = styled.div`
+  display: flex;
+  background-color: #F6D1E5;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 4px;
+`
+const DeleteButton = styled.button`
+  margin-left: auto;
+  background-color: #F6D1E7;
+  border: none;
+  color: #B4889E;
+`
 
 export const Task = ({ taskData }) => {
   const dispatch = useDispatch()
   return (
-    <div>
+    <TaskContainer>
 
       <input type='checkbox' checked={taskData.complete} onChange={() => dispatch(tasks.actions.toggleItem(taskData.id))} />
 
       {taskData.text}
 
-      <button className="deleteItem" onClick={() => dispatch(tasks.actions.deleteItem(taskData.id))}>
-        Delete
-    </button>
+      <DeleteButton className="deleteItem" onClick={() => dispatch(tasks.actions.deleteItem(taskData.id))}>
+        x
+    </DeleteButton>
 
-    </div>
+    </TaskContainer>
   )
 }
