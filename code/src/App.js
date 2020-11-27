6fccbbd7d7ad9b2f661720d39a58ab7261f7b403
Rebkova <rebkova@gmail.com>
2020-11-27 13:30:14 +0100

    removed unused style

--- code/src/App.js
+++ code/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Provider } from "react-redux"
 import { combineReducers, createStore } from "@reduxjs/toolkit"
-import style from "styled-components"
 
 //--- reducer ---
 import { todoTasks } from './reducer/todoTasks'
