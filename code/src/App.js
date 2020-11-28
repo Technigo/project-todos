0c70f56e17a2e4f5be46022270ccdbdf34cb6c3a
ClaireCaudwell <clairecaudwell@gmail.com>
2020-11-28 17:49:12 +0100

    Implemented local storage so when the user returns to the todo app then the todos previously added are still visible in the list.

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, createStore } from '@reduxjs/toolkit';
 
 import { Tasks } from 'reducer/Tasks';
 import { TaskHeader } from 'components/TaskHeader';
@@ -16,8 +16,25 @@ const reducer = combineReducers({
   tasks: Tasks.reducer
 });
 
-/* Store creation: Takes all the reducers (could be one or a few different js files) from the combineReducers and creating the store using the reducers and the retrieved state. Then you can access these different stores your components as being made accessible through the provider */
-const store = configureStore({ reducer });
+//New store code
+// 1. Retrieve the local storage and use it as the initial state
+// PersistedStateJSON will get a text value from localstorage in the browser with a key called reduxstate (can be called whatever you want). Just going to hold values. If the persistedStateJSON exists in the localStorage then we'll assign that to the variable persistedState
+const persistedStateJSON = localStorage.getItem('todos-reduxstate');
+let persistedState = {};
+
+if (persistedStateJSON) {
+  persistedState = JSON.parse(persistedStateJSON);
+}
+
+// 2. Create the store using the initial state
+// Then we pass in the value that's in the persistedStore, which is the initialstate, and the reducer to the creatStore function to create the store. This will be different from the initalstate specified in the redux store. 
+const store = createStore(reducer, persistedState, window._REDUX_DEVTOOLS_EXTENSION_&& window._REDUX_DEVTOOLS_EXTENSION_());
+
+// 3. Store the state in localstorage on any redux state change
+// Now we're telling the store to subscribe to every event that happens in the store. After the actions happen then the state will be written out to local storage and the JSON.stringify will return it as a readable string for us to access in the provider below
+store.subscribe(() => {
+  localStorage.setItem('todos-reduxstate', JSON.stringify(store.getState()));
+});
 
 export const App = () => {
   return (
