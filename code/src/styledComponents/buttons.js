a888cf8d86cb9d221dd58d18ac68ebd6dbd9c977
ELLEN BJORKMAN <ELLEN881008@HOTMAIL.SE>
2020-11-28 13:23:31 +0100

    tries to fix styling issue with buttons

--- code/src/styledComponents/buttons.js
+++ code/src/styledComponents/buttons.js
@@ -1,17 +1,5 @@
 import styled from 'styled-components'
 
-export const AddButton = styled.button`
-  background: black;
-  color: white;
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-  font-size: 30px;
-  margin-left: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
 export const DeleteButton = styled.button`
   background: black;
   color: white;
@@ -23,5 +11,11 @@ export const DeleteButton = styled.button`
   border-radius: 3px;
 `
 export const DeleteAllButton = styled(DeleteButton)`
+  margin-left: 20px;
+`
+export const AddButton = styled(DeleteButton)`
+  width: 40px;
+  height: 40px;
+  font-size: 30px;
   margin-left: 20px;
 `
--- code/src/styledComponents/form.js
+++ code/src/styledComponents/form.js
@@ -1,45 +1,5 @@
 import styled from 'styled-components'
 
-/*Checkboxes */
-/*Hidden checkbox with actual input.*/
-export const HiddenCheckBox = styled.input.attrs({type:'checkbox'})`
-  border: 0;
-  clip: rect(0 0 0 0 );
-  clip-path: inset(50%);
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  padding: 0;
-  position: absolute;
-  white-space: nowrap;
-  width: 1px;
-`
-/*checkmark */
-export const Icon = styled.svg`
-  fill: none; 
-  stroke: white;
-  stroke-width: 2px;
-`
-/*New styled checkbox */
-export const StyledCheckbox = styled.div`
-  display: inline-block;
-  width: 20px;
-  height: 20px;
-  background: ${props => (props.checked ? 'black' : 'lightgrey')};
-  border: ${props => (props.checked ? 'none' : 'black 0.5px solid')};
-  border-radius: 3px;
-  transition: all 150ms;
-  ${Icon} { /* displays depending on if the state variable is checked or not*/
-    visibility: ${(props) => (props.checked ? 'visible' : 'hidden')}
-  }
-  ${HiddenCheckBox}:focus + & {
-    outline: 2px dotted black;
-  }
-`
-export const CheckboxContainer = styled.div`
-  display: inline-block;
-  vertical-align: middle;
-`
 /* Inputfield */
 export const Form = styled.form`
   width: 100%;
