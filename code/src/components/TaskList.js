1cf2c2ad619895cd594ae349c1d669831a6a1d0c
emma-tech <emma.urman@gmail.com>
2020-11-28 15:06:05 +0100

    Added first component, TaskList.js, which will fetch the list of todos and map over them.

---
+++ code/src/components/TaskList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import useSelector here
+
+// TaskList component
+export const TaskList = () => {
+
+    return (
+        <div>
+            Here is where the tasklist will be. 
+        </div>
+    )
+}
