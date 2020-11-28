4947fcd10b4abee4532be2f8af36de2586da14ef
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2020-11-28 23:49:26 +0100

    app structure decided

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,23 @@
+App
 import React from 'react'
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+import { Info } from './components/Info'
+import { Form } from './components/Form'
+import { List } from './components/List';
+import { todos } from './reducers/todos';
+
+const reducer = combineReducers({ todos: todos.reducer });
+const store = configureStore({ reducer });
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    return (
+        <Provider store={store}>
+            <Info />
+            <Form />
+            <List />
+        </Provider>
+    );
+};
+
