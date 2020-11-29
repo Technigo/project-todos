a538088c5af6f4a2b77ba6c076f8ca58834465e9
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2020-11-29 14:44:07 +0100

    changed font-size of title

--- code/src/lib/Title.js
+++ code/src/lib/Title.js
@@ -4,14 +4,14 @@ export const Title = styled.h1`
   color: ${(props) => props.color || "black"};
   font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
     monospace;
-  font-size: 25px;
+  font-size: 18px;
   padding: 0px 10px;
   margin: ${(props) => props.margin || "16px 0px"};
 `;
 
 export const Subtitle = styled.h2`
   color: ${(props) => props.color || "black"};
-  font-size: 15px;
+  font-size: 10px;
   font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
     monospace;
   padding: 0px 10px;
