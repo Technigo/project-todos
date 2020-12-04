24d684131ee889ac307b88b7f9a1d387923bffad
Annika Melin <melin.anki>
2020-12-04 14:17:21 +0100

    updated to non mutated code in reducer

--- code/src/reducers/todos.js
+++ code/src/reducers/todos.js
@@ -18,20 +18,28 @@ export const todos = createSlice({
   initialState,
   reducers: {
     addTask: (state, action) => {
-      state.tasks.push({ id: idCounter += 1, text: action.payload, isComplete: false })
-      // push adds one item to the end of the existing array
+      state.tasks = [...state.tasks, { id: idCounter += 1, text: action.payload, isComplete: false }]
+      // we are setting current state of tasks to a new array, containing the added task in the end
+      // ...state.tasks creates a new array containing content from the original array
+
+      // original code: state.tasks.push({ id: idCounter += 1, text: action.payload, isComplete: false })
+      // push adds one item to the end of the existing array, and mutates the existing array
+      // immer handles the mutability issue for us, so this is ok with immer - but a bad habit, even though it works
     },
     removeTask: (state, action) => {
       state.tasks = state.tasks.filter(task => task.id !== action.payload)
-      // filter creates a new array
+      // filter creates a new array based on the provided condition
+      // as filter is creating a new array hence being immutable by nature, this code is ok
     },
     updateTask: (state, action) => {
       const updatedTask = state.tasks.find(task => task.id === action.payload.id)
       // find returns value of the first element that satisfies the condition
       if (action.payload.isComplete === false) {
-        updatedTask.isComplete = true
+        state.tasks = [...state.tasks]; // create new array not to mutate original array, works without this due to immer - but bad habit code
+        updatedTask.isComplete = true;
       } else if (action.payload.isComplete === true) {
-        updatedTask.isComplete = false
+        state.tasks = [...state.tasks];
+        updatedTask.isComplete = false;
       }
     }
   }
