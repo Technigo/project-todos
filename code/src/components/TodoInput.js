d7d6ac6be5110df6f282ae79017f77a12afb629d
destinyleia <destinylindgrenc@gmail.com>
2021-02-19 18:23:38 +0100

    required updated

--- code/src/components/TodoInput.js
+++ code/src/components/TodoInput.js
@@ -25,10 +25,12 @@ export const TodoInput = () => {
         type="text"
         onChange={e => setInputValue(e.target.value)}
         value={inputValue}
+        required
         className="todo-input-text"
       ></input>
       <input
         type="submit"
+        required
         className="todo-input-bottom"
         value="Add"
       ></input>
