7bf87bcf2c94f5c333d3c2b8f775e79de4452a4a
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-04-19 23:19:56 +0200

    basic structure

---
+++ code/src/reducers/todos.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+/* eslint-disable linebreak-style */
+import { createSlice } from '@reduxjs/toolkit'
+
+const todos = createSlice({
+    name: 'todos',
+    initialState: {
+        items: [
+           { id: 1, description: 'First task', isComplete: false},
+           { id: 2, description: 'Second task', isComplete: true}, 
+           { id: 3, description: 'Third task', isComplete: false} 
+
+        ]
+
+    },
+    reducers: {
+        toggleComplete: (store, action) => {
+            console.log(action)
+            const updatedItems = store.items.map(todo => {
+                if (todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        isComplete: !todo.isComplete
+                    }
+
+                } else {
+                    return todo
+
+                }
+            })
+            store.items = updatedItems
+        }
+    }
+})
+
+export default todos
