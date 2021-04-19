c50400893a21d574aeaff47e99ee622a3b852e65
Hannah Jesinkey <h.jesinkey@gmail.com>
2021-04-19 20:11:14 +0200

    After first lecture

---
+++ code/src/reducers/todos.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const todos = createSlice({
+  name: 'todos',
+  initialState: {
+    items: [
+      { id: 1, description: 'Test', isComplete: false },
+      { id: 2, description: 'Test2', isComplete: true },
+      { id: 3, description: 'Test3', isComplete: false },
+    ],
+  },
+  reducers: {
+    toggleComplete: (store, action) => {
+      const updatedItems = store.items.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            isComplete: !todo.isComplete,
+          };
+        } else {
+          return todo;
+        }
+      });
+      store.items = updatedItems;
+    },
+  },
+});
+
+export default todos;
