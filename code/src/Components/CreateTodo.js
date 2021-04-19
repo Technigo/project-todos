3a61ba99683669415f5ca0ecf7f283bab4abd459
Pauan8 <pauline.j.andersson@vgregion.se>
2021-04-19 22:29:14 +0200

    added custom checkbox and other styling

--- code/src/Components/CreateTodo.js
+++ code/src/Components/CreateTodo.js
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
-import {useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { todos } from "../Reducers/todos";
+import "./CreateTodoStyle.css";
 
 export const CreateTodo = () => {
   const [newTask, setNewTask] = useState("");
   const dispatch = useDispatch();
 
+  const handleClick = () => {
+    dispatch(todos.actions.createNewTodo({ task: newTask }));
+    setNewTask("");
+  };
+
   return (
     <section className="new-task">
-      <button
-        type="button"
-        onClick={() =>
-          dispatch(todos.actions.createNewTodo({ task: newTask }))
-        }
-      >
+      <button type="button" onClick={() => handleClick()}>
         +
       </button>
       <input
-        type="text"
+        type="textarea"
+        placeholder="Add task"
         value={newTask}
         onChange={(e) => setNewTask(e.target.value)}
       ></input>
--- code/src/Components/TodoList.js
+++ code/src/Components/TodoList.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { todos } from "../Reducers/todos";
+import "./TodoListStyle.css";
 
 export const TodoList = () => {
   const tasks = useSelector((store) => store.todos.items);
@@ -10,15 +11,27 @@ export const TodoList = () => {
   return (
     <section className="todo-list">
       {tasks.map((task) => (
-        <div key={task.id} className="todo-list__item">
-          <input
-            id={task.id}
-            type="checkbox"
-            onChange={() =>
-              dispatch(todos.actions.toggleComplete({ id: task.id }))
+        <div key={task.id} className="todo-list-item">
+          <label
+            className="todo-list-item__label"
+            htmlFor={task.id}
+            style={
+              task.isComplete
+                ? { color: "rgb(196, 232, 241)" }
+                : { color: "black" }
             }
-          ></input>
-          <label htmlFor={task.id}>{task.description}</label>
+          >
+            <input
+              checked={task.isComplete}
+              id={task.id}
+              type="checkbox"
+              onChange={() =>
+                dispatch(todos.actions.toggleComplete({ id: task.id }))
+              }
+            />
+            <span className="todo-list-item__checkbox-mark"></span>
+            {task.description}
+          </label>
         </div>
       ))}
     </section>
