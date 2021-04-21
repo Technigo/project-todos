85423ac909700a71dc4ad42cade076ed3387f47e
Bellsibub <isabella.billgren@gmail.com>
2021-04-21 12:40:37 +0200

    added compl. list to task list component

--- code/src/components/TaskList/index.js
+++ code/src/components/TaskList/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Container } from '@material-ui/core';
+import { Container, Button } from '@material-ui/core';
+import { ArrowDropDown, ArrowDropUp } from '@material-ui/icons';
 import Task from 'components/Task';
 import { useStyles } from './style';
 
 export default () => {
   const classes = useStyles();
-  const tasks = useSelector((state) => state.tasks.list)
+  const [complListOpen, setComplListOpen] = useState(false);
+  const uncomplTasks = useSelector((state) => state.tasks.list.filter((t) => !t.complete));
+  const complTasks = useSelector((state) => state.tasks.list.filter((t) => t.complete));
+
   return (
     <Container className={classes.root}>
-      {tasks.map((task) => <Task key={task.id} {...task} />)}
+      {uncomplTasks.map((task) => (
+        <Task key={task.id} {...task} />
+      ))}
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setComplListOpen(!complListOpen)}>
+        Complete
+        {complListOpen ? <ArrowDropDown /> : <ArrowDropUp />}
+        {complTasks.length}
+      </Button>
+      {complListOpen && complTasks.map((task) => (
+        <Task key={task.id} {...task} />
+      ))}
     </Container>
   );
 };
--- code/src/components/TaskList/style.js
+++ code/src/components/TaskList/style.js
@@ -3,6 +3,6 @@ import { makeStyles } from '@material-ui/styles';
 export const useStyles = makeStyles(() => ({
   root: {
     overflowY: 'scroll',
-    height: 'calc(100vh - 200px)'
+    height: 'calc(100vh - 300px)'
   }
 }));
