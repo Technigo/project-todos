86e089ae5c6d3170a3613f9121027fd28365f481
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-04-22 18:46:15 +0200

    Updated filter function with useEffect

--- code/src/components/TodoList.js
+++ code/src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import EmptyTodoList from './EmptyTodoList'
@@ -10,20 +10,38 @@ import FilterThumb from './FilterThumb'
 const TodoList = () => {
   const items = useSelector((store) => store.todo.items)
   const filters = useSelector(store => store.todo.filters)
+  const [filteredItimes, setFilteredItems] = useState([])
   //console.log(filters)
-  let filteredItimes = [...items]
-  filters.forEach((filter) => {
-    if(filter.name === "all done" && filter.status){
-       filteredItimes = filteredItimes.filter(item => item.isComplete)
-    }
-    if(filter.name === "all undone" && filter.status) {
-        filteredItimes = filteredItimes.filter(item => !item.isComplete) 
-    }
-    if(filter.name === "past deadline" && filter.status) {
-       filteredItimes = filteredItimes.filter(item => (new Date(item.deadline) < Date.now()))
-    }
-  })
-  console.log(filteredItimes)
+  // let filteredItimes = [...items]
+  // filters.forEach((filter) => {
+  //   if(filter.name === "all done" && filter.status){
+  //      filteredItimes = filteredItimes.filter(item => item.isComplete)
+  //   }
+  //   if(filter.name === "all undone" && filter.status) {
+  //       filteredItimes = filteredItimes.filter(item => !item.isComplete) 
+  //   }
+  //   if(filter.name === "past deadline" && filter.status) {
+  //      filteredItimes = filteredItimes.filter(item => (new Date(item.deadline) < Date.now()))
+  //   }
+  // })
+  // console.log(filteredItimes)
+
+  useEffect(() => {
+    let newFilteredItems = [...items]
+    filters.forEach(filter => {
+      if (filter.name === "all done" && filter.status) {
+        newFilteredItems = (newFilteredItems.filter(item => item.isComplete))
+      }
+      if (filter.name === "all undone" && filter.status) {
+        newFilteredItems = (newFilteredItems.filter(item => !item.isComplete))
+      }
+      if (filter.name === "past deadline" && filter.status) {
+        newFilteredItems = (newFilteredItems.filter(item => (new Date(item.deadline) < Date.now())))
+      }
+    })
+    setFilteredItems(newFilteredItems)
+  }, [filters, items])
+
 
   return (
     <div className="list-container">
@@ -33,7 +51,7 @@ const TodoList = () => {
         <div className="filters-container">
           <ClearAllButton />
           <AllDoneButton />
-          {filters.map((filter) => <FilterThumb key={filter.id} filter={filter}/>)}
+          {filters.map((filter) => <FilterThumb key={filter.id} filter={filter} />)}
         </div>
       }
 
