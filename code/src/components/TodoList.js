4678dafb1a075ac17674c53641d08dcf3f0b03b9
johlofg <jh.lofgren@gmail.com>
2021-04-22 17:05:18 +0200

    added opacity to completed tasks

--- code/src/components/TodoList.js
+++ code/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import styled from 'styled-components'
 
 import todos from 'reducers/todos'
 
@@ -12,20 +13,20 @@ const TodoList = () => {
   return (
     <div className="todo-container">
       <div className="counter-container">
-        <p className="task-counter">Total tasks: {items.length}</p>
-        <p className="task-counter">Remaing tasks: {incompleteTask.length}</p>
+        <p>Total tasks: {items.length}</p>
+        <p>Remaing tasks: {incompleteTask.length}</p>
       </div>
       <div className="item-container">
         {items.map(todo => (
-        <div key={todo.id} className="todo-item">
+        <div key={todo.id} className="todo-item" style={ todo.isComplete ? {opacity: 0.5} : {opacity: 1}}>
           <p className="todo-task grid-item-1">{todo.description}</p>
           <div className="checkbox-complete grid-item-2">
-            <label htmlFor="isComplete">Complete: </label>
+            <label htmlFor="checkboxLabel">Complete: </label>
             <input 
               type="checkbox"            
               checked={todo.isComplete}
               onChange={() => dispatch(todos.actions.toggleComplete(todo.id))}
-              name="isComplete"
+              name="checkboxLabel"
             />
           </div>
           <div className="grid-item-3">
