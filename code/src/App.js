2586613a25df90b7d937bf42d363d01a6addb14e
newmarkdev <newmarkdev@gmail.com>
2021-04-23 11:20:12 +0200

    updated todo count

--- code/src/App.js
+++ code/src/App.js
@@ -1,27 +1,25 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import todos from './reducers/todos'
 import TodoList from './components/TodoList'
 import TodoForm from './components/TodoForm'
 import TotalCompleteItems from './components/TotalCompleteItems'
 
-
-
 const reducer = combineReducers({
   todos: todos.reducer
 })
 
-const store = configureStore({reducer});
+const store = configureStore({ reducer });
 
 export const App = () => {
   return (
     <div className="todo-list">
-      <Provider store={store} >
-            <TodoForm />
-            <TodoList />
-            <TotalCompleteItems />
+      <Provider store={store}>
+        <TodoForm />
+        <TodoList />
+        <TotalCompleteItems />
       </Provider>
     </div>
   )
--- code/src/components/TotalCompleteItems.js
+++ code/src/components/TotalCompleteItems.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-
-
-
 const TotalCompleteItems = () => {
-	const todos = useSelector((store) => store.todos.items);
-	const completedTodos = todos.filter(singleTodo => singleTodo.isComplete)
+  const todos = useSelector((store) => store.todos.items);
+  const completedTodos = todos.filter((singleTodo) => singleTodo.isComplete)
 
-	return (
-	<h2>Completed: {completedTodos.length}</h2>
-	)
-	
+  return (
+    <h2>Completed: {completedTodos.length}/{todos.length}</h2>
+  )
 };
-
 
 export default TotalCompleteItems;
