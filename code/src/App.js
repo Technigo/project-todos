8b2bac7e36af8de9eb14b0a17be726a4f6f2d7a2
johlofg <jh.lofgren@gmail.com>
2021-04-23 22:24:35 +0200

    changed design in desktop view

--- code/src/App.js
+++ code/src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import todos from './reducers/todos'
+
 import TodoList from './components/TodoList'
 import TodoForm from './components/TodoForm'
 
@@ -14,9 +15,11 @@ const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <Provider store={store}>      
-      <TodoForm />
-      <TodoList />      
+    <Provider store={store}> 
+      <main className="main">     
+        <TodoForm />
+        <TodoList /> 
+      </main>     
     </Provider>
   )
 }
