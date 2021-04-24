8c0fb025c0bd8fc2b8d446a52e15acb3157c235e
M4L5 <malin.gu2017@gmail.com>
2021-04-24 20:50:37 +0200

    removed a form that was not used

--- code/src/App.js
+++ code/src/App.js
@@ -4,7 +4,6 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import todos from './reducers/todos';
 import TodoList from './components/TodoList';
-import Form from './components/Form' 
 
 const reducer = combineReducers({
   todos: todos.reducer
