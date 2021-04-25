417f1b1ee69075bebabc3381b701197a003f1237
Anna Honkonen <ak.honkonen@gmail.com>
2021-04-25 15:35:01 +0300

    solved deploy log warnings

--- code/src/components/TodoInformation.js
+++ code/src/components/TodoInformation.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import TaskCounter from './TaskCounter'
 
 const TodoInformation = () => {
-  const [startDate, setStartDate] = useState(new Date())
   return (
     <div className="todo-information-container">
       <h1 className="main-title">Todo</h1>
