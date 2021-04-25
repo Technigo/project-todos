a8174104e54e9161e83c9fc38434ac8250206ab7
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-04-25 10:27:35 +0200

    testng styling

--- code/src/compononets/Header.js
+++ code/src/compononets/Header.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Moment from 'react-moment';
 
 const Header = () => {
   const tasks = useSelector((store) => store.tasks.items);
   const completedCount = tasks.filter((task) => task.complete === true).length;
+  const date = new Date()
 
   return (
     <div className="header">
       <div className="left-header">
         <h1>My</h1>
         <h1>Tasks</h1>
+        <Moment format="MMMM DD, YYYY" className='date'>{date}</Moment>
       </div>
       <div className="right-header">
         <h2>{tasks.length}</h2>
         <p className='count-paragraph'>tasks</p>
-        <p className='count-percentage'>{((completedCount / tasks.length) * 100).toFixed(0)} % done</p>
+        {tasks.length === 0 ? <p className='count-percentage'>0 % done</p> : <p className='count-percentage'>{((completedCount / tasks.length) * 100).toFixed(0)} % done</p>}
       </div>
     </div>
   );
