a38054384b56c63f3d845e4a523f993982e32880
Mir Albertioni <fethullah.dincer@gmail.com>
2021-05-07 16:25:43 +0200

    working with deploying

--- code/src/components/EmptyPage.js
+++ code/src/components/EmptyPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Emptypage = () => {
     return (
         <p className="empty-page">
-            It seems like you don't have any Todos at the moment. Take your time and enjoy the moment &#128513;
+            It seems like you don't have any Todos at the moment. Take your time and enjoy the moment <span>&#128513;</span>
         </p>
     )
 }
