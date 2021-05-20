18100afc0f159be862fb1b794a0f4a4c9dab7456
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-05-20 12:12:15 +0200

    Added more responsiveness and fixed missed indentation issues

--- code/src/components/AllDoneButton.js
+++ code/src/components/AllDoneButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 import todo from '../reducers/todo'
 
--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -12,23 +12,25 @@ const Header = () => {
   const itemsNotDone = items.filter(item => !item.isComplete)
 
   return (
-    <div className="header-container">
-      <div className="date-wrapper">
-        <h1 className="header-heading">TO DO</h1>
-        <p className="week-day">{moment().format('dddd')} </p>
-        <p>{moment().format('MMMM Do')} </p>
-      </div>
-      {items.length > 0 &&
-        <div className="done-undone-wrapper">
-          <p className="done-text">{itemsDone.length} tasks done</p>
-          <p className="undone-text">{itemsNotDone.length} tasks to do</p>
-          <div className="clear-buttons">
-            <ClearAllButton />
-            <AllDoneButton />
-            <ResetButton />
-          </div>
+    <div className="header-wrapper">
+      <div className="header-container">
+        <div className="date-wrapper">
+          <h1 className="header-heading">TO DO</h1>
+          <p className="week-day">{moment().format('dddd')} </p>
+          <p>{moment().format('MMMM Do')} </p>
         </div>
-      }
+        {items.length > 0 &&
+          <div className="done-undone-wrapper">
+            <p className="done-text">{itemsDone.length} tasks done</p>
+            <p className="undone-text">{itemsNotDone.length} tasks to do</p>
+            <div className="clear-buttons">
+              <ClearAllButton />
+              <AllDoneButton />
+              <ResetButton />
+            </div>
+          </div>
+        }
+      </div>
     </div>
   )
 }
