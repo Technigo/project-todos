48dec320259b4b7fb3bb9f97bc1900eab8a96e29
M4L5 <malin.gu2017@gmail.com>
2021-07-02 14:08:13 +0200

    imported material ui, added more styled-components to TodoList.js

--- code/src/components/TodoList.js
+++ code/src/components/TodoList.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
 
-import styled from 'styled-components/macro'
+import styled from 'styled-components/macro';
+
+import TextField from '@material-ui/core/TextField'
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
 
 import todos from '../reducers/todos';
 
@@ -11,69 +18,57 @@ const TodoList = () => {
   const completedItems = items.filter(singleTodo => singleTodo.isComplete);
   const addLabel = useSelector((store) => store.todos.addLabel);
   const dispatch = useDispatch();
-
-  const TodoContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-  `
-
-  const TodoWrapper = styled.section`
-    background-color: #F9F0F4;
-    max-width: 400px;
-    margin: 50px 20px;
-    padding: 30px 20px;
-  `
-
-  const TodoHeader = styled.header`
-    
-  `
-
     
     return (
       <TodoContainer>
         <TodoWrapper>
           <TodoHeader>
-            <h1>To do list</h1>
-            <div className="completed-tasks">
-              <p>Completed: {completedItems.length}/{items.length}</p>
-            </div>
-            </TodoHeader>
+            <Title>Malin's To do list</Title>
+            <CompletedSection>
+              <CompletedTasks>Completed: {completedItems.length}/{items.length}</CompletedTasks>
+            </CompletedSection>
+          </TodoHeader>
             <div className="user-input">
-              <input
+              <TextField
+                required
+                variant="standard"
+                type="text"
                 className="add-todo"
                 placeholder="Add to do here "
                 id="todoLabel"
                 onChange={(e) => dispatch(todos.actions.setAddLabel(e.target.value))}
                 value={addLabel}
               />
-              <input 
-                className="add-todo-button"
-                type="button"
-                value="add to do"
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                size="small"
+                startIcon={<SaveIcon />}
+                aria-label="add todo"
                 onClick={() => {
                   dispatch(todos.actions.addToDo()) 
                 }}
-              />
+              > Save
+              </Button>
             </div>
 
             {items.map(todo =>  (
-              <div className="user-output" key={todo.id}>
-                <input
-                  className="checkbox"
+              <UserInput key={todo.id}>
+                <Checkbox
                   type="checkbox"
+                  color="primary"
                   checked={todo.isComplete}
                   onChange={() => dispatch(todos.actions.toggleComplete(todo.id))}
                 />
                 <p className="todo-description">{todo.description}</p>
-                  <input
-                    className="delete-button"
-                    type="button"
-                    value="delete"
+                  <IconButton
+                    aria-label="remove"
                     onClick={() => dispatch(todos.actions.removeToDo(todo.id))}
-                    />
-              </div>
+                  >
+                    <DeleteIcon fontSize="small"/>
+                  </IconButton>
+              </UserInput>
             ))}
         </TodoWrapper>
       </TodoContainer>
@@ -81,3 +76,38 @@ const TodoList = () => {
 };
 
 export default TodoList;
+
+const TodoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const TodoWrapper = styled.section`
+  background-color: #D8EEFE;
+  max-width: 400px;
+  margin: 50px 20px;
+  padding: 30px 20px;
+  border-radius: 20px;
+`
+
+const TodoHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`
+
+const Title = styled.h1`
+`
+
+const CompletedSection = styled.section`
+`
+
+const CompletedTasks = styled.p`
+
+`
+
+const UserInput = styled.section`
+  display: flex;
+  flex-direction: row;
+`
