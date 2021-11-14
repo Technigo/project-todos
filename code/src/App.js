0b2d91730532ec6bf370acc8f9320cf1d26d157a
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-11-14 22:45:51 +0100

    added component files and tasks slice

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+
+import { Tasks } from 'components/Tasks'
+//a reducer required for the store
+
+import { tasks } from 'reducers/tasks'
+
+
+// This will combine all reducers
+const reducer = combineReducers({
+  tasks: tasks.reducer,
+})
+
+//use the single reducer to create a Store
+const store = configureStore({reducer})
 
 export const App = () => {
+  
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    // need to wrap everything in provider
+  <Provider store={store}>
+    Hey hey, we are ready for your tasks
+  <Tasks/>
+
+  </Provider>
+  
+)}
---
+++ code/src/components/Tasks.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export const Tasks = () => {
+    return (
+        <div> Here is my to-do list</div>
+    )
+}
