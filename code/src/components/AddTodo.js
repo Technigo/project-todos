b3ba09a7ac91614fdfdec3b4d46527312a68639e
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-11-15 20:46:45 +0100

    reducers implemeted, delete, add and check functionallity added.

---
+++ code/src/components/AddTodo.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { todos } from "../reducers/todos";
+
+export const AddTodo = () => {
+  const dispatch = useDispatch();
+
+  const [input, setInput] = useState("");
+
+  const onAddTodo = () => {
+    dispatch(todos.actions.addTodo(input));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      <button onClick={onAddTodo}>Add</button>
+    </div>
+  );
+};
---
+++ code/src/reducers/todos.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+import uniqid from "uniqid";
+
+export const todos = createSlice({
+  name: "todos",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addTodo: (store, action) => {
+      const newTodo = {
+        id: uniqid(),
+        text: action.payload,
+        isComplete: false,
+      };
+      store.items = [...store.items, newTodo];
+    },
+    toggleTodo: (store, action) => {
+      const updatedItem = store.items.map((item) => {
+        if (item.id === action.payload) {
+          const updatedTodo = {
+            ...item,
+            isComplete: !item.isComplete,
+          };
+          return updatedTodo;
+        } else {
+          return item;
+        }
+      });
+      store.items = updatedItem;
+    },
+    deleteTodo: (store, action) => {
+      const decreasedItems = store.items.filter(
+        (item) => item.id !== action.payload
+      );
+      store.items = decreasedItems;
+    },
+  },
+});
