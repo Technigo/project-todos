e866bdad08449ac484eaabc5d0867e43873d3bdc
Ida Aspen <ida.aspen@outlook.com>
2021-11-15 20:34:31 +0100

    finished counter in Summary component

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import './summary.css';
 // number of active todos, if no todos "YAY you are free"
 // counter of done tasks
 export const Summary = () => {
+  const tasksLength = useSelector((store) => store.todos.items.length);
+
   return (
     <section className="summary-wrapper">
-      <h2>12</h2>
+      <h2>{tasksLength}</h2>
       <h3>tasks for today</h3>
       <p>2 tasks done</p>
     </section>
