5825262c410fe0e98033fc416289285e5eacfb7f
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2021-11-15 21:17:25 +0100

    Added components, reducers and comments.

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Provider } from 'react-redux'; 
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+import todos from './reducers/todos'; //our slice
+
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+
+//app.js are the origin component of our app.
+
+const reducer = combineReducers({ //a varibale that collects all of our slices
+	todos: todos.reducer, //here we specify all our slices
+});
+
+const store = configureStore({ reducer }); //reducer is the property that configuterStore expects from us
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+	return (
+		//Our components
+		<Provider store={store}> 
+			<AddTodo />
+			<TodoList />
+		</Provider>
+	);
+};
---
+++ code/src/reducers/todos.js
@@ -0,0 +1,60 @@
+import { createSlice } from '@reduxjs/toolkit';
+import uniqid from 'uniqid';
+
+//our first and only slice - a chunk from our global store
+
+const todos = createSlice({ //name the varibale as the slice
+	name: 'todos', //3 properties, name, initalState and reducers
+	initialState: {
+		items: [], //empty array of our todos
+	},
+
+	//Reducers are always provided with two arguments, one is the store (or the current value of the store). The other is action, some information that we send from component. 
+
+
+	reducers: {
+		addTodo: (store, action) => { //Is an object where each property is just one method to update the store
+			const newTodo = {
+				id: uniqid(), //uniqe id fron uniqe id npm
+				text: action.payload,
+				isComplete: false,
+			};
+
+
+
+			// v2 Immutability approach. Prepare new glass and add what ever was in the old glass and add one other extra thing (store.items)
+			store.items = [...store.items, newTodo];
+		},
+		toggleTodo: (store, action) => {
+
+			// v2 Immutability approach
+			const updatedItems = store.items.map((item) => {
+				if (item.id === action.payload) {
+					const updatedTodo = {
+						// id: item.id,
+						// text: item.text,
+						// isComplete: item.isComplete,
+						...item,
+						isComplete: !item.isComplete,
+					};
+					return updatedTodo;
+				} else {
+					return item;
+				}
+			});
+
+			store.items = updatedItems;
+		},
+		deleteTodo: (store, action) => {
+
+			// v2 Immutability approach (id in action.payload)
+			const decreasedItems = store.items.filter(
+				(item) => item.id !== action.payload
+			);
+
+			store.items = decreasedItems;
+		},
+	},
+});
+
+export default todos;
