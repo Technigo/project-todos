f226be9a3d8a51bdf6ed5ef252edaa317dfc0405
Ida Aspen <ida.aspen@outlook.com>
2021-11-15 20:57:25 +0100

    completed a message if there is no data

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './summary.css';
-// number of active todos, if no todos "YAY you are free"
-// counter of done tasks
+
 export const Summary = () => {
   const tasksLength = useSelector((store) => store.todos.items.length);
 
-  return (
-    <section className="summary-wrapper">
-      <h2>{tasksLength}</h2>
-      <h3>tasks for today</h3>
-      <p>2 tasks done</p>
-    </section>
-  );
+  if (tasksLength < 1) {
+    return (
+      <section className="summary-wrapper-empty">
+        <h2>HOORAY </h2>
+        <h3>you are free!!!</h3>
+        <p>- or be a bit productive and add moore moore moore tasks...</p>
+      </section>
+    );
+  } else {
+    return (
+      <section className="summary-wrapper">
+        <h2>{tasksLength}</h2>
+        <h3>tasks for today</h3>
+        <p>2 tasks done</p>
+      </section>
+    );
+  }
 };
 
 export default Summary;
