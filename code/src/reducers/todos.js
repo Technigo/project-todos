0b58c50fdf8095044bd89bb5aa7c4f181d59aef8
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2021-11-16 13:42:09 +0100

    basic structures done, added timestamp on post, started styling

---
+++ code/src/reducers/todos.js
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit'
+import moment from 'moment'
+import uniqid from 'uniqid'
+
+const todos = createSlice({
+    name: 'todos',
+    initialState: {
+        items: []
+    },
+    reducers: {
+        addTodo: (store, action) => {
+            const newTodo = {
+                id: uniqid(),
+                text: action.payload,
+                isComplete: false,
+                timePosted: moment().subtract(10, 'days').calendar(),
+            }
+
+            store.items = [...store.items, newTodo]
+        },
+        toggleTodo: (store, action) => {
+            const updatedItems = store.items.map(item => {
+                if (item.id === action.payload) {
+                    const updatedTodo = {
+                        ...item,
+                        isComplete: !item.isComplete
+                    }
+                    return updatedTodo
+                } else {
+                    return item
+                }
+            })
+            store.items = updatedItems
+        },
+        deleteTodo: (store, action) => {
+            const decreasedItems = store.items.filter(item => item.id !== action.payload)
+
+            store.items = decreasedItems
+        }
+    }
+})
+
+export default todos
