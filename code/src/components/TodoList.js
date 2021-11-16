22a2787fffaa9c352ad8b9110b432ce69dd6d1a3
e-delsol <80672920+e-delsol@users.noreply.github.com>
2021-11-16 17:18:08 +0100

    Setting up the project, creating text-input, add todo-button, checkbox and delete-button

---
+++ code/src/components/TodoList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { todos } from "../reducers/todos";
+
+export const TodoList = () => {
+	const items = useSelector((store) => store.todos.items);
+
+	const dispatch = useDispatch();
+
+	const onToggleTodo = (id) => {
+		dispatch(todos.actions.toggleTodo(id));
+	};
+
+	const onDeleteTodo = (id) => {
+		dispatch(todos.actions.deleteTodo(id));
+	};
+
+	return (
+		<section>
+			{items.map((item, index) => (
+				<div className="flex-item" key={item.id}>
+					<input
+						type="checkbox"
+						checked={item.isComplete}
+						onChange={() => onToggleTodo(item.id)}
+					/>
+					<p>{item.text}</p>
+					<button onClick={() => onDeleteTodo(item.id)}>Delete</button>
+				</div>
+			))}
+		</section>
+	);
+};
