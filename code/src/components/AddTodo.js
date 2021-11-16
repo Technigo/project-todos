5475cad176a7b6a06bd720ac61f4937d246a5e37
Maria Thomasson <81348201+MariaThomasson@users.noreply.github.com>
2021-11-16 06:40:38 +0100

    Indentations organized

--- code/src/components/AddTodo.js
+++ code/src/components/AddTodo.js
@@ -4,24 +4,24 @@ import { useDispatch } from 'react-redux'
 import todos from '../reducers/todos'
 
 const AddTodo = () => {
-	const [input, setInput] = useState('')
+  const [input, setInput] = useState('')
 
-	const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-	const onAddTodo = () => {
-		dispatch(todos.actions.addTodo(input))
-	}
+  const onAddTodo = () => {
+    dispatch(todos.actions.addTodo(input))
+  }
 
-	return (
-		<div>
-			<input
-				type="text"
-				value={input}
-				onChange={(event) => setInput(event.target.value)}
-			/>
-			<button onClick={onAddTodo}>Add todo</button>
-		</div>
-	)
+  return (
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      <button onClick={onAddTodo}>Add todo</button>
+    </div>
+  )
 }
 
 export default AddTodo
--- code/src/reducers/todos.js
+++ code/src/reducers/todos.js
@@ -2,9 +2,9 @@ import { createSlice } from '@reduxjs/toolkit'
 import uniqid from 'uniqid'
 
 const todos = createSlice({
-	name: 'todos',
-	initialState: {
-		items: [
+  name: 'todos',
+  initialState: {
+    items: [
       {
         id: 1,
         text: "Complete Project",
@@ -24,44 +24,44 @@ const todos = createSlice({
         createdDate: 1606493967957,
       },
     ],
-	},
-	reducers: {
-		addTodo: (store, action) => {
-			const newTodo = {
-				id: uniqid(),
-				text: action.payload,
-				isComplete: false,
-			}
+  },
+  reducers: {
+    addTodo: (store, action) => {
+      const newTodo = {
+        id: uniqid(),
+        text: action.payload,
+        isComplete: false,
+      }
       // Immutability approach
       store.items = [...store.items, newTodo]
-		},
+    },
 
     toggleTodo: (store, action) => {
-		  // Immutability approach
-			const updatedItems = store.items.map((item) => {
-				if (item.id === action.payload) {
-					const updatedTodo = {
-						...item,
-						isComplete: !item.isComplete,
-					}
-					return updatedTodo
-				} else {
-					return item
-				}
-			})
+      // Immutability approach
+      const updatedItems = store.items.map((item) => {
+        if (item.id === action.payload) {
+          const updatedTodo = {
+            ...item,
+            isComplete: !item.isComplete,
+          }
+          return updatedTodo
+        } else {
+          return item
+        }
+      })
 
-			store.items = updatedItems
-		},
+      store.items = updatedItems
+    },
 
-		deleteTodo: (store, action) => {
-		  // Immutability approach (id in action.payload)
-			const decreasedItems = store.items.filter(
-				(item) => item.id !== action.payload
-			)
+    deleteTodo: (store, action) => {
+      // Immutability approach (id in action.payload)
+      const decreasedItems = store.items.filter(
+        (item) => item.id !== action.payload
+      )
 
-			store.items = decreasedItems
-		}
-	}
+      store.items = decreasedItems
+    }
+  }
 })
 
 export default todos
