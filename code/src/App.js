bbba2e4adb5df6b9b73ee9020eaa2d339491b162
Rephili <72921631+Rephili@users.noreply.github.com>
2021-11-16 21:53:15 +0100

    started with the project

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import todos from "./reducers/todos";
+
+const reducer = combineReducers({
+  todos: todos.reducer,
+});
+
+const store = configureStore({ reducer: reducer });
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <Provider store={store}>
+      <div>hello technigo</div>
+    </Provider>
+  );
+};
