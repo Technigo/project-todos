11c966a8b13d968dd6c1d75aeadb83bae5af699e
mamite100 <mamite.andersson@student.kyh.se>
2021-11-17 23:36:45 +0100

    strctur of first todo

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit'; 
 
+import todos from './reducers/todos';
+
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+
+const reducer = combineReducers({
+  todos: todos.reducer
+});
+
+const store = configureStore({
+  reducer
+});
 export const App = () => {
   return (
-    <div>
-      Hej
-    </div>
-  )
-}
+   <Provider store={store}>
+			<AddTodo />
+			<TodoList />
+		</Provider>
+  );
+  };  
