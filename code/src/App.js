d8a7eb818793463d39551d6ba042739b5757a26f
Rephili <72921631+Rephili@users.noreply.github.com>
2021-11-17 20:33:50 +0100

    finished the basic setup

--- code/src/App.js
+++ code/src/App.js
@@ -4,16 +4,20 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import todos from "./reducers/todos";
 
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+
 const reducer = combineReducers({
   todos: todos.reducer,
 });
 
-const store = configureStore({ reducer: reducer });
+const store = configureStore({ reducer });
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <div>hello technigo</div>
+      <AddTodo />
+      <TodoList />
     </Provider>
   );
 };
--- code/src/reducers/todos.js
+++ code/src/reducers/todos.js
@@ -1,11 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
+import uniqid from "uniqid";
 
 const todos = createSlice({
   name: "todos",
   initialState: {
     items: [],
   },
-  reducers: {},
+  reducers: {
+    addTodo: (store, action) => {
+      const newTodo = {
+        id: uniqid(),
+        text: action.payload,
+        isComplete: false,
+      };
+
+      store.items = [...store.items, newTodo];
+    },
+
+    toggleTodo: (store, action) => {
+      const updatedItems = store.items.map((item) => {
+        if (item.id === action.payload) {
+          const updatedTodo = {
+            ...item,
+            isComplete: !item.isComplete,
+          };
+          return updatedTodo;
+        } else {
+          return item;
+        }
+      });
+      store.items = updatedItems;
+    },
+    deleteTodo: (store, action) => {
+      const decreasedItems = store.items.filter(
+        (item) => item.id !== action.payload
+      );
+      store.items = decreasedItems;
+    },
+  },
 });
 
 export default todos;
