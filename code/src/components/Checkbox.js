731846d8b8c9ce1bb1ce59c1b4667c568ac1c194
Sarah Mottram <smelbows@gmail.com>
2021-11-18 16:54:18 +0100

    styling

--- code/src/components/Checkbox.js
+++ code/src/components/Checkbox.js
@@ -29,12 +29,13 @@ const StyledCheckbox = styled.div`
     display: inline-block;
   width: 35px;
   height: 35px;
-  background: ${props => props.checked ? 'salmon' : 'papayawhip'};
+  background: ${props => props.checked ? '#4CD622' : '#FFF'};
   border-radius: 50%;
   transition: all 150ms;
+  border: 1px solid #B5B5B5;
 
   ${HiddenCheckbox}:focus + & {
-    box-shadow: 0 0 0 3px pink;
+    box-shadow: 0 0 0 3px #FBF3D5;
   }
 
   ${Icon} {
--- code/src/components/ToDoList.js
+++ code/src/components/ToDoList.js
@@ -8,20 +8,45 @@ import Checkbox from "./Checkbox";
 const StyledContainer = styled.div`
     width: 320px;
     margin: auto;
+    background-color: #F0F0F0;
 `;
 
 const StyledToDoItem = styled.div`
-    border-bottom: solid gray 1px;
+    border-bottom: solid #B5B5B5 1px;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
     gap: 10px;
+    margin: 10px;
 `;
 
 const StyledTaskText = styled.p`
 flex-grow: 1;
+`
+const StyledButton = styled.button`
+    box-shadow:inset 0px 1px 0px 0px #f5978e;
+	background:linear-gradient(to bottom, #f24537 5%, #f7a49c 100%);
+	background-color:#f24537;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	color:#ffffff;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #810e05;
 
+    &:hover {
+        background:linear-gradient(to bottom, #f7a49c 5%, #f24537 100%);
+	background-color:#f7a49c;
+    }
+
+    &:active {
+        position:relative;
+	top:1px;
+    }
 `
 
 const ToDoList = () => {
@@ -51,13 +76,13 @@ const ToDoList = () => {
                         ></Checkbox>
                     </label>
                     <StyledTaskText>{item.text}</StyledTaskText>
-                    <button
+                    <StyledButton
                         onClick={() => {
                             onDelete(item.id);
                         }}
                     >
-                        Delete task
-                    </button>
+                        Delete
+                    </StyledButton>
                 </StyledToDoItem>
             ))}
         </StyledContainer>
