afd2d927ca396abd03d01a3e02ffcf258a27ebfc
Sarah Mottram <smelbows@gmail.com>
2021-11-18 12:23:31 +0100

    added isComplete and Delete functionality

--- code/src/components/ToDoList.js
+++ code/src/components/ToDoList.js
@@ -1,12 +1,45 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
+import todos from "reducers/todos";
+
 const ToDoList = () => {
     const allTodos = useSelector((state) => state.todos.items);
 
     const dispatch = useDispatch();
 
-    return <div>{allTodos.map((item) => item.text)}</div>;
+    const onCompletedToggle = (id) => {
+        dispatch(todos.actions.toggleCompleted(id));
+    };
+
+    const onDelete = (id) => {
+        dispatch(todos.actions.deleteToDo(id));
+    };
+
+    return (
+        <div>
+            {allTodos.map((item) => (
+                <div key={item.id}>
+                    <p>{item.text}</p>
+                    <input
+                        type="checkbox"
+                        checked={item.isComplete}
+                        onChange={() => {
+                            onCompletedToggle(item.id);
+                        }}
+                    >
+                    </input>
+                    <button
+                        onClick={() => {
+                            onDelete(item.id);
+                        }}
+                    >
+                        Delete To-Do
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default ToDoList;
