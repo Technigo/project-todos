d20474de745caca1458d7094cb75c20f06f048df
MarySnopok <marysnopok8@gmail.com>
2021-11-18 19:21:27 +0100

    change for brush container

--- code/src/components/BackgroundSelection.js
+++ code/src/components/BackgroundSelection.js
@@ -4,17 +4,19 @@ export const BackgroundSelection = styled.div`
 max-width: 220px;
 display flex; 
 flex-direction: row;
-position: fixed: 
-left:220px;
-right: 0;
+position: relative;
 align-items: center;
 justify-content: flex-start;
 font-size: 16px;
 
 @media (min-width: 769px) {
     max-width: 700px;
-   left:500px;
-   right: 0;
+  
     font-size: 18px;
   }
 `;
+
+// left: px;
+// right: 0;
+// left:500px;
+// right: 0;
