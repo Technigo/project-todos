719f7d220305ce14c795f0d78864ad1f2e51024b
MarySnopok <marysnopok8@gmail.com>
2021-11-18 19:27:59 +0100

    width fix

--- code/src/components/BackgroundSelection.js
+++ code/src/components/BackgroundSelection.js
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 
 export const BackgroundSelection = styled.div`
-max-width: 220px;
-display flex; 
-flex-direction: row;
-position: relative;
-align-items: center;
-justify-content: flex-start;
-font-size: 16px;
+  max-width: 220px;
+  display flex;
+  flex-direction: row;
+  position: relative;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 16px;
 
-@media (min-width: 769px) {
+  @media (min-width: 769px) {
     max-width: 700px;
-  
+
     font-size: 18px;
   }
 `;
