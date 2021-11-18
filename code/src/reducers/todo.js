89a1dd89d59749b505db38d7e1fa8a99366d2228
themisk84 <efthymios.karakasis@gmail.com>
2021-11-18 10:44:52 +0100

    start again

---
+++ code/src/reducers/todo.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+import uniqid from "uniqid";
+
+const todo = createSlice({
+  name: "todo",
+  initialState: {
+    list: [],
+  },
+  reducers: {
+    addTodo: (store, action) => {
+      console.log(action.payload);
+      const NewTodo = {
+        id: uniqid(),
+        text: action.payload,
+        isComplete: false,
+        date: Date.now(),
+      };
+
+      store.list = [...store.list, NewTodo];
+    },
+
+    toggleStatus: (store, action) => {
+      const updatedList = store.list.map((item) => {
+        if (item.id === action.payload) {
+          const updatedTodo = {
+            ...item,
+            isComplete: !item.isComplete,
+          };
+          return updatedTodo;
+        } else return item;
+      });
+      store.list = updatedList;
+    },
+
+    removeTodo: (store, action) => {
+      const removeTodos = store.list.filter(
+        (item) => item.id !== action.payload
+      );
+      store.list = removeTodos;
+    },
+
+    removeAll: (store) => {
+      store.list = [];
+    },
+  },
+});
+
+export default todo;
