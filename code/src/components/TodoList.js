0602dd361ac7a731d8da487641c8798d15af4bbd
silvertejp89 <kriss.stockhaus@gmail.com>
2021-11-18 15:28:12 +0100

    codealong leture, some styling

---
+++ code/src/components/TodoList.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import todos from "../reducers/todos";
+
+const TodoList = () => {
+  const items = useSelector((store) => store.todos.items);
+
+  const dispatch = useDispatch();
+
+  const onToggleTodo = (id) => {
+    dispatch(todos.actions.toggleTodo(id));
+  };
+
+  const onDeleteTodo = (id) => {
+    dispatch(todos.actions.deleteTodo(id));
+  };
+
+  return (
+    <section>
+      {items.map((item) => (
+        <div className="todo-container" key={item.id}>
+          <div className="todo-content">
+            <div className="todo">
+              <input
+                type="checkbox"
+                checked={item.isComplete}
+                onChange={() => onToggleTodo(item.id)}
+              />
+              <p>{item.text}</p>
+            </div>
+            <button
+              className="delete-button"
+              onClick={() => onDeleteTodo(item.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default TodoList;
---
+++ code/src/reducers/todos.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+import uniqid from "uniqid";
+
+const todos = createSlice({
+  name: "todos",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addTodo: (store, action) => {
+      console.log(action);
+      const data = action.payload;
+
+      const newTodo = {
+        id: uniqid(),
+        text: action.payload,
+        isComplete: false,
+      };
+
+      // Immutability approach
+      store.items = [...store.items, newTodo];
+    },
+    toggleTodo: (store, action) => {
+      // Immutability approach
+      const updatedItems = store.items.map((item) => {
+        if (item.id === action.payload) {
+          const updatedTodo = {
+            ...item,
+            isComplete: !item.isComplete,
+          };
+          return updatedTodo;
+        } else {
+          return item;
+        }
+      });
+
+      store.items = updatedItems;
+    },
+    deleteTodo: (store, action) => {
+      //mutability approach
+      // store.items.splice(action.payload, 1);
+
+      // Immutability approach
+      const decreasedItems = store.items.filter(
+        (item) => item.id !== action.payload
+      );
+
+      store.items = decreasedItems;
+    },
+  },
+});
+
+export default todos;
