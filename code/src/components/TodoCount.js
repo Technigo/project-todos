e0cc32be24ccb9b5335bd830f1bbfe61257bf09e
Jsfrulle <Jessicamhermansson@gmail.com>
2021-11-19 10:37:36 +0100

    fix

--- code/src/components/TodoCount.js
+++ code/src/components/TodoCount.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-scroll";
-import { useDispatch } from "react-redux";
+
 const TodoCount = () => {
 
  
