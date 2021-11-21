badda7b52707a948702b46260d708dd30216e9f2
vrill <52007698+vrill@users.noreply.github.com>
2021-11-21 17:32:13 +0100

    Initial commit. Created a slice for the todo's and imported the basics.

---
+++ code/src/reducers/todos.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todos = createSlice({
+    name: 'todos',
+    initialState: {
+        items: []
+    },
+    reducers: {
+
+    },
+})
+
+export default todos
