789f6477c12d3e2aa8054b7ef6fd1cf171c04de8
fatigr26 <fatigr2692@gmail.com>
2021-11-21 22:22:09 +0100

    fixed errors console

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
 import styled from "styled-components";
 import img from "../images/mountain-background.jpeg";
 
-//a reducer required for sending info via dispatch
-import todos from "../reducers/todos";
 
 //Styled component
 const HeaderTag = styled.header`
