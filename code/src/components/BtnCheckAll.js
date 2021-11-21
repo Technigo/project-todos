a44ba09eb97625db93905bdb5b931278852cbc55
Jessica Nygren <jessicanygren@C02D54BJMD6R.local>
2021-11-21 22:40:05 +0100

    console error, correction

--- code/src/components/BtnCheckAll.js
+++ code/src/components/BtnCheckAll.js
@@ -1,10 +1,9 @@
 import React from "react"
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 import todos from '../reducers/todos'
 
 
 const BtnCheckAll = () => {
-    const items = useSelector((store) => store.todos.items)
 
     const dispatch = useDispatch()
 
