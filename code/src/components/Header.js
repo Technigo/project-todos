768a56b8c60fd1a1dfc02a98e34247dbaff9b8ef
Gustav Frid <gustav.frid@gmail.com>
2021-11-22 12:08:52 +0100

    code cleanup

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import styled from 'styled-components/macro'
+import styled from 'styled-components'
 import moment from 'moment'
 
 import { todos } from '../reducers/todos'
