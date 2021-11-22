928f73baf4959b01fbbcf7abcdec28821b08c59b
Harry Bäcklin <83465217+Groovedharry@users.noreply.github.com>
2021-11-22 12:00:26 +0100

    Updated the code
    removed some code

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-// import todoSlice from 'reducers/todoSlice';
+
 
 import './Header.css';
 
@@ -10,7 +10,7 @@ export const Header = () => {
   return (
     <header className="header">
       <h1>To-do List</h1>
-      {/* <p>To keep you from forgetting what you have to do</p> */}
+      
       <p className="todo-count">
       {todos.length} left
       </p>
