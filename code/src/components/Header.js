d45947aa0800cde45047a61ee72b826fc6b0b136
Linnea <linnea.iseb@gmail.com>
2021-11-22 14:49:07 +0100

    Fixed the amount left in header

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -5,15 +5,16 @@ import { useSelector } from 'react-redux';
 import './Header.css';
 
 export const Header = () => {
-  const todos = useSelector((state) => state.todoSlice.todoList);
+  const todos = useSelector((state) => state.todoSlice.todoList.length);
+  const completedTodos = useSelector(
+    (state) => state.todoSlice.todoList.filter((item) => !item.done).length
+  );
 
   return (
     <header className="myheader">
       <h1>To-do-list</h1>
       <p>This is a to-do list. Enter what you need to do and check it off once it's finished!</p>
-      <p className="todo-count">
-       {todos.length} left to do
-      </p>
+      <p className="todo-count">{completedTodos} of {todos} left to do</p>
     </header>
   );
 };
