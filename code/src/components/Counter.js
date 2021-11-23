cf8887f4c008ce68d7b8f17dfaedf0ff0af3d150
NinaAlejandra <Niinaa___@hotmail.com>
2021-11-23 19:07:32 +0100

    changed margins and added an other counter

--- code/src/components/Counter.js
+++ code/src/components/Counter.js
@@ -4,7 +4,16 @@ import "./counter.css";
 
 const Counter = () => {
   const items = useSelector((store) => store.todos.items.length);
-  return <p className="task-amount">you have {items} tasks </p>;
+  const amount = useSelector(
+    (store) => store.todos.items.filter((item) => item.isComplete).length
+  );
+
+  return (
+    <div className="tasks">
+      <p className="task-amount">you have {items} tasks </p>
+      <p className="task-amount-done">you have completed {amount} tasks </p>
+    </div>
+  );
 };
 
 export default Counter;
