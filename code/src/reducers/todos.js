40abafc4e7049a87b29cab65686e362102d96f06
molbimien <43146046+molbimien@users.noreply.github.com>
2021-12-10 13:03:16 +0100

    setup of project and added code to add, toggle and delete tasks from a task list

---
+++ code/src/reducers/todos.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit"
+import uniqid from 'uniqid'
+
+const todos = createSlice({
+     name: 'todos',
+     initialState: {
+         items: []
+     }, 
+     reducers: {
+        addTodo: (store, action) => {
+            console.log(action)
+
+            const newTodo = {
+                id: uniqid(),
+                text: action.payload,
+                isComplete: false
+            }
+
+            store.items = [...store.items, newTodo ]
+         },
+        toggleTodo: (store, action) => {
+            console.log(action)
+
+            const updatedItems = store.items.map(item => {
+                if (item.id === action.payload) {
+                    const updatedTodo = {
+                        ...item,
+                        isComplete: !item.isComplete
+                    }
+                    return updatedTodo
+                } else {
+                    return item
+                }
+            })
+
+            store.items = updatedItems
+        }, 
+        deleteTodo: (store, action) => {
+            const decreasedItems = store.items.filter(item => item.id !== action.payload)
+
+            store.items = decreasedItems
+        }
+    },
+})
+
+export default todos
