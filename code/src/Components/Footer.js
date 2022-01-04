c6438558a700ceb762d1b1f9ac82fb3d2fa70dfc
HedvigM <hedvig@mejstedt.se>
2022-01-04 14:59:20 +0100

    links in the footer

--- code/src/Components/Footer.js
+++ code/src/Components/Footer.js
@@ -1,18 +1,77 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faMountain } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab);
 
 export const Footer = () => {
   return (
     <StyledFooter>
       <h1>
-        Hedvig Mejstedt{' '}
-        <span aria-label="lion" role="img">
-          🦁
-        </span>
+        Hedvig Mejstedt
+        <br />
+        <Icons>
+          <Icon
+            href="https://github.com/HedvigM"
+            target="_blank"
+            tabindex="0"
+            rel="noreferrer"
+            aria-label="a link to my GitHub account">
+            <FontAwesomeIcon icon={['fab', 'github']} />
+          </Icon>
+          <Icon
+            href="https://www.linkedin.com/in/hedvig-mejstedt"
+            target="_blank"
+            tabindex="0"
+            rel="noreferrer"
+            aria-label="a link to my linkedin account">
+            <FontAwesomeIcon icon={['fab', 'linkedin']} />
+          </Icon>
+          <Icon
+            href="https://stackoverflow.com/users/16650863/hedvig"
+            target="_blank"
+            tabindex="0"
+            rel="noreferrer"
+            aria-label="a link to my stack-overflow account">
+            <FontAwesomeIcon icon={['fab', 'stack-overflow']} />
+          </Icon>
+          <Icon
+            href="https://mejstedt.se"
+            target="_blank"
+            tabindex="0"
+            rel="noreferrer"
+            aria-label="a link to my portfolio">
+            <FontAwesomeIcon icon={faMountain} />
+          </Icon>
+        </Icons>
       </h1>
     </StyledFooter>
   );
 };
+
+const Icon = styled.a`
+  margin: 5px 5px;
+  color: black;
+  cursor: pointer;
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -webkit-transition-property: transform;
+  transition-property: transform;
+
+  :hover {
+    -webkit-transform: scale(1.5);
+    transform: scale(1.5);
+  }
+`;
+
+const Icons = styled.div`
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+`;
 
 const StyledFooter = styled.footer`
   border: 5px dotted black;
