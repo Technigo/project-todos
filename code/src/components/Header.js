756a0227019f1d3ea4d4635b34f600a7c989689c
Lena Simma <84474447+lenisili@users.noreply.github.com>
2022-01-10 13:25:14 +0100

    Update Header.js

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -2,11 +2,11 @@ import React from "react"
 import styled from "styled-components/macro"
 
 const TitleContainer = styled.div`
-    display: flex;
-    margin: 30px auto 20px 300px;
-    color: white;
-    font-weight: bold;
-    font-size: 48px;
+    display: flex
+    margin: 30px auto 20px 300px
+    color: white
+    font-weight: bold
+    font-size: 48px
 `
 
 const Header = () => {
