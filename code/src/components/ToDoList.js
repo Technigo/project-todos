7c564252cc1de630de224591d55f5a592fe0a88c
Lena Simma <84474447+lenisili@users.noreply.github.com>
2022-01-10 13:27:25 +0100

    Update ToDoList.js

--- code/src/components/ToDoList.js
+++ code/src/components/ToDoList.js
@@ -18,59 +18,59 @@ const ToDoList = () => {
         dispatch(todos.actions.deleteTodo(index))
     }
 
-    const Container = styled.section`
-    display: flex;
-    align-items: center;
-    width: 100%;
-    margin-bottom: 8px;
-    `
-    const TodoItem = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex-grow: 2;
-    background-color: #edf5e1;
-    height: 32px;
-    width: 80%;
-    margin-left: 5px;
-    margin-right: 10px;
-    margin-top; 5px;
-    border-radius: 5px;
-    `
+const Container = styled.section`
+    display: flex
+    align-items: center
+    width: 100%
+    margin-bottom: 8px
+ `
+const TodoItem = styled.div`
+    display: flex
+    flex-direction: column
+    flex-grow: 2
+    background-color: #edf5e1
+    height: 32px
+    width: 80%
+    margin-left: 5px
+    margin-right: 10px
+    margin-top; 5px
+    border-radius: 5px
+  `
 
-    const Deletebutton = styled.button`
-    border: none;
-    background-color: #379683;
-    padding: 8px 8px;
-    border-radius: 5px;
-    font-weight: 700;
-    color: white;
-    width: 65px;
-    `
-    const Checkbox = styled.input`
-    margin-left: 25px;
+const Deletebutton = styled.button`
+    border: none
+    background-color: #379683
+    padding: 8px 8px
+    border-radius: 5px
+    font-weight: 700
+    color: white
+    width: 65px
+`
+const Checkbox = styled.input`
+    margin-left: 25px
     margin-right: 5px;
-    display: inline-block;
-    width: 32px;
-    height: 32px;
-    background: #05386B;
-    border-radius: 5px;
-    `
+    display: inline-block
+    width: 32px
+    height: 32px
+    background: #05386B
+    border-radius: 5px
+`
 
-    //displaying all the possible actions
-    return (<section>
-        {items.map((item, index) =>(
-            <Container key={item.id}>
-                <Checkbox
-                    type="checkbox" 
-                    checked={item.isComplete} 
-                    onChange={() => onToggleTodo(item.id)}
-                />
-                <TodoItem>{item.text}</TodoItem>
-                <Deletebutton onClick={() => onDeleteTodo(index)}>Delete</Deletebutton>
-            </Container>
-        ))}
-    </section>
-    )
+ //displaying all the possible actions
+return (<section>
+    {items.map((item, index) =>(
+        <Container key={item.id}>
+            <Checkbox
+                type="checkbox" 
+                checked={item.isComplete} 
+                onChange={() => onToggleTodo(item.id)}
+            />
+            <TodoItem>{item.text}</TodoItem>
+            <Deletebutton onClick={() => onDeleteTodo(index)}>Delete</Deletebutton>
+        </Container>
+    ))}
+</section>
+)
 }
 
 export default ToDoList
