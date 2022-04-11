72fc8b31110dcef300d9ee338ddea37efe142afb
Emma Lindell <emma.lindell4@gmail.com>
2022-04-11 21:19:32 +0200

    Removed motion

--- code/src/components/StartPage.js
+++ code/src/components/StartPage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import Completed from 'components/Completed'
-import { motion } from 'framer-motion'
 
 import taskList from 'reducers/tasks'
 
