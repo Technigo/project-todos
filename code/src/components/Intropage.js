da4c918bc78edf1f47ca7943bc676e92915a5d3c
Emma Lindell <emma.lindell4@gmail.com>
2022-04-11 20:18:57 +0200

    Image startPage

--- code/src/components/Intropage.js
+++ code/src/components/Intropage.js
@@ -1,26 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-
-
+import styled from 'styled-components'
+  
 const IntroPage = () => {
 
     return (
 
         <>
-        <div className="cloudy-header">
-            <img src="./assets/cloud.png" alt="clouds" />
-        <h1 className="header-start">Do it today – get it out of your head</h1>
-        </div>
+       <img src="./assets/todo-illustration.png" alt="Illustration made by pch.vector - www.freepik.com" />
+
+        <h1 className="header-start">
+            Do it today – get it out of your head
+        </h1>
 
         <Link to="/app">
-            <button className="btn">Write it down</button>
+            <Button>Write it down</Button>
         </Link>
         </>
-
-
     )
 }
 
 
 export default IntroPage
+
+
+//styled components
+
+const Cloud = styled.img`
+position: relative;
+background: url('./assets/cloud-green.png');
+`
+
+const Button = styled.button`
+background: transparent;
+border-radius: 10px;
+font-family: 'Della Respira', serif;
+font-size: 14px;
+border: 1px solid gray;
+padding: 0.3rem;
+`
