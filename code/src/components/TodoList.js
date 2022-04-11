26b94b56da8365e578981b6eda8c68c12cfbad86
LovisaBrorson <lovisabrorson@gmail.com>
2022-04-11 17:33:41 +0200

    added 2 components and 2 reducers with code

---
+++ code/src/components/TodoList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import todos from "../reducers/todos";
+
+const TodoList = () => {
+  const items = useSelector((store) => store.todos.items);
+  const dispatch = useDispatch();
+  const onToggleToDo = (id) => {
+    dispatch(todos.actions.toggleTodo(id));
+  };
+
+  const onDeleteTodo = (index) => {
+    dispatch(todos.actions.deleteTodo(index));
+  };
+
+  return (
+    <section>
+      {items.map((item, index) => (
+        <div className="flex-item" key={item.id}>
+          <p>{item.text}</p>
+          <input
+            type="checkbox"
+            checked={item.isComplete}
+            onChange={() => onToggleToDo(item.id)}
+          />
+          <button onClick={() => onDeleteTodo(index)}>Delete</button>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default TodoList;
