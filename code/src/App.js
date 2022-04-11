636591b24c7627f8eadad1a68e8eff660323bc9b
Emma Lindell <emma.lindell4@gmail.com>
2022-04-11 20:55:36 +0200

    Added 404-page

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import StartPage from 'components/StartPage'
 import IntroPage from 'components/Intropage'
 import tasks from 'reducers/tasks'
+import NotFound from 'components/NotFound'
 
 
 const reducer = combineReducers({
@@ -23,6 +24,8 @@ export const App = () => {
           <Routes>
             <Route path="/" element={<IntroPage />} />
             <Route path="/app" element={<StartPage />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
           </Routes>
         </BrowserRouter>
    </Provider>
---
+++ code/src/components/NotFound.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { useNavigate } from 'react-router-dom'
+import styled from "styled-components"
+
+const NotFound = () => {
+
+    const navigate= useNavigate()
+    const onHomeButtonClick = () => {
+        navigate('/')
+    }
+
+    return (
+        <div>
+            <img src="./assets/sorry.jpg" alt="illustration created by storyset, www.freepik.com" width="300px"/>
+            <p>
+               <h2> Ooopsi, this page does not exist. Try again! </h2>
+                <Button onClick={onHomeButtonClick}>Return to Home Page</Button>
+            </p>
+
+        </div>
+
+    )
+
+}
+
+export default NotFound
+
+//styled components
+
+const Button = styled.button`
+background: transparent;
+border-radius: 10px;
+font-family: 'Della Respira', serif;
+font-size: 14px;
+border: 1px solid gray;
+padding: 0.3rem;
+`
