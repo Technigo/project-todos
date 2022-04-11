d782cb3dc0962c525d4660ab8b6bf4fb84ae042f
Emma Lindell <emma.lindell4@gmail.com>
2022-04-11 16:49:08 +0200

    path again

--- code/src/components/Intropage.js
+++ code/src/components/Intropage.js
@@ -8,7 +8,7 @@ const IntroPage = () => {
         <>
         <h1>Do it today – get it out of your head</h1>
 
-        <a href="/StartPage">
+        <a href="/StartPage.js">
             <button className="btn">Write it down</button>
         </a>
         </>
