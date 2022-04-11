2a7de5e50b02aeb055c953a45963c2a31ee5e4a5
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-04-11 15:13:17 +0200

    basic structure

---
+++ code/src/components/AddTodo.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import todos from '../reducers/todos';
+
+const AddTodo = () => {
+	const [input, setInput] = useState('');
+
+	const dispatch = useDispatch();
+
+	const onAddTodo = () => {
+		dispatch(todos.actions.addTodo(input));
+	};
+
+	return (
+		<div>
+			<input
+				type='text'
+				value={input}
+				onChange={(event) => setInput(event.target.value)}
+			/>
+			<button onClick={onAddTodo}>Add todo</button>
+		</div>
+	);
+};
+
+export default AddTodo;
---
+++ code/src/reducers/todos.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit';
+import uniqid from 'uniqid';
+
+const todos = createSlice({
+	name: 'todos',
+	initialState: {
+		items: [],
+	},
+	reducers: {
+		addTodo: (store, action) => {
+			const newTodo = {
+				id: uniqid(),
+				text: action.payload,
+				isComplete: false,
+			};
+
+			store.items = [...store.items, newTodo];
+		},
+		toggleTodo: (store, action) => {
+			// v1 Mutability approach
+			// store.items.forEach((item) => {
+			// 	if (item.id === action.payload) {
+			// 		item.isComplete = !item.isComplete;
+			// 	}
+			// });
+			//v2 Imutability approach
+			const updatedItems = store.items.map((item) => {
+				if (item.id === action.payload) {
+					const updatedTodo = {
+						...item,
+						isComplete: !item.isComplete,
+					};
+					return updatedTodo;
+				} else {
+					return item;
+				}
+			});
+			store.items = updatedItems;
+		},
+		deleteTodo: (store, action) => {
+			const decreasedItems = store.items.filter(
+				(item) => item.id !== action.payload
+			);
+			store.items = decreasedItems;
+		},
+	},
+});
+
+export default todos;
+
+// const increaseFunction = () => {
+// 	setCounter(counter + 1);
+// };
+
+// const decreaseFunction = () => {
+// 	setCounter(counter - 1);
+// };
