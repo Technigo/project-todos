00c0577525dfb2c70a71923d859edfe854c52f17
Emma Lindell <emma.lindell4@gmail.com>
2022-04-11 16:02:17 +0200

    Added IntroPage

--- code/src/App.js
+++ code/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import TasksList from 'components/TaskList'
 import pokemons from 'reducers/pokemons.js'
+import IntroPage from 'components/Intropage.js'
 
 const reducer = combineReducers({
   pokemons: pokemons.reducer,
@@ -14,10 +16,12 @@ reducer: reducer,
 
 export const App = () => {
   return (
-
     <Provider store={store}>
-      <TasksList />
-
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IntroPage />} />
+        <Route path="/app" element={<TasksList />} />
+      </Routes>
+      </BrowserRouter>
    </Provider>
-
   )}
---
+++ code/src/components/Intropage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const IntroPage = () => {
+
+    return (
+
+        <>
+        <h1>Do it today – Get it out of your head</h1>
+
+        <a href="/app"><button>Write it down</button></a>
+        </>
+
+
+    )
+}
+
+
+export default IntroPage
