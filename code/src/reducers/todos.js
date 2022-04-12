b165875936ca8a73e5d3ec5d8fc01764c6179bb2
Jenny Fiskaare <jennyfiskaare@Jennys-MacBook-Air.local>
2022-04-12 22:06:09 +0200

    added items data

--- code/src/reducers/todos.js
+++ code/src/reducers/todos.js
@@ -3,9 +3,33 @@ import { createSlice } from "@reduxjs/toolkit";
 const todos = createSlice ( {
 name: 'todos',
 initialState: {
-    items: []
+    items: [
+        {
+            id: '1',
+            task: 'Eat',
+            isComplete: 'false',
+            display:'true'
+        },
+        {
+            id: '2',
+            task: 'Sleep',
+            isComplete: 'true',
+            display:'true'
+        },
+        {
+            id: '3',
+            task: 'Clean',
+            isComplete: 'false',
+            display:'false'
+        },
+        {
+            id: '4',
+            task: 'Workout',
+            isComplete: 'true',
+            display:'false'
+        }
+    ]
 };
-
 
 });
 
