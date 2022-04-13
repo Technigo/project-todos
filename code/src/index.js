91c74fe05e281d5e0b14807fdfe77237e0b144b7
Arianna Allahyar <anallahyar@gmail.com>
2022-04-13 10:46:15 +0200

    Initial store created

--- code/src/index.js
+++ code/src/index.js
@@ -1,6 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { App } from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
+import { store } from "app.store.js";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
