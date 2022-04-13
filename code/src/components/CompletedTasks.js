4072d4a5e9b80d02718a31d1fbf88de63478bb1a
idanaslund <idanaslund@users.noreply.github.com>
2022-04-13 10:06:20 +0200

    map in completed tasks

--- code/src/components/CompletedTasks.js
+++ code/src/components/CompletedTasks.js
@@ -1,14 +1,41 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux"
+
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 
 const CompletedTasks = () => {
+    const allTasks = useSelector((store) => store.tasks.items)
+
+    const dispatch = useDispatch()
+
+    // const onItemToggle = (taskId) => {
+    //     dispatch(tasks.actions.toggleItem(taskId))
+    // }
+
+
     return (
         <main>
             <Header />
                 <section>
-                This is where I want to put completed tasks.
-                </section>
+            <div>
+            {allTasks.map(taskItem => (
+                <article key={taskItem.id}>
+                    <h3>{taskItem.text}</h3>
+                    <label> Completed:
+                        <input type="checkbox" 
+                        checked={taskItem.complete} 
+                        // onChange={() => onItemToggle(taskItem.id)}
+                        />
+                    </label>
+                    <button type="button">                      
+                     {/* onClick={() =>onButtonClick(taskItem.id)} */}
+                        <span role="img" aria-label="cross">❌
+                        </span></button>
+                </article>
+            ))}
+            </div>
+        </section>
             <Footer />
         </main>
     )
