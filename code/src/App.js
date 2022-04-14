7f602b5b0a485a2a75d4ad7201d8476f4d4ad2e0
Camilla Hallberg <camillahallberg@Camillas-MacBook-Pro.local>
2022-04-14 14:09:19 +0200

    misspelled path

--- code/src/App.js
+++ code/src/App.js
@@ -6,7 +6,7 @@ import todos from 'reducers/todos'
 
 import AddTodo from './components/AddTodo'
 import TodoList from './components/TodoList'
-import ToDoCounter from 'components/TodoCounter'
+import ToDoCounter from './components/TodoCounter'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
