257727c832c049c354e5fb73c84ed358b0b0c09c
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-04-14 13:06:22 +0200

    removed moment

--- code/src/reducers/todos.js
+++ code/src/reducers/todos.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 import uniqid from 'uniqid';
-import moment from 'moment';
 
 const todos = createSlice({
 	name: 'todos',
@@ -13,19 +12,11 @@ const todos = createSlice({
 				id: uniqid(),
 				text: action.payload,
 				isComplete: false,
-				timePosted: moment().format('DD MMMM YYYY, HH:mm'),
 			};
 
 			store.items = [...store.items, newTodo];
 		},
 		toggleTodo: (store, action) => {
-			// v1 Mutability approach
-			// store.items.forEach((item) => {
-			// 	if (item.id === action.payload) {
-			// 		item.isComplete = !item.isComplete;
-			// 	}
-			// });
-			//v2 Imutability approach
 			const updatedItems = store.items.map((item) => {
 				if (item.id === action.payload) {
 					const updatedTodo = {
@@ -49,11 +40,3 @@ const todos = createSlice({
 });
 
 export default todos;
-
-// const increaseFunction = () => {
-// 	setCounter(counter + 1);
-// };
-
-// const decreaseFunction = () => {
-// 	setCounter(counter - 1);
-// };
