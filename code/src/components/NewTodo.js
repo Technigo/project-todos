3e14fa60e5462585f4c8e5561b91cf701c27c274
rijad90 <rijad.i@hotmail.com>
2022-04-15 21:50:29 +0200

    updated reducer, added a buttom, added some styled components

--- code/src/components/NewTodo.js
+++ code/src/components/NewTodo.js
@@ -1,8 +1,55 @@
 import React from "react"
+import { useSelector, useDispatch } from "react-redux"
+import styled from "styled-components"
+
+import todos from "reducers/todos"
+
+const Todo = styled.article`
+  border: 1px solid #dcdcdc;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 5px;
+  position: relative;
+`
+const DeleteButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  border: none;
+  background: none;
+  cursor: pointer;
+`
 
 //functional component
 const NewTodo = () => {
-  return <section> New todo will be here..</section>
+  const newTodo = useSelector((store) => store.todos.items)
+  const dispatch = useDispatch()
+  const onTodoToggle = (todoId) => {
+    dispatch(todos.actions.toggleItem(todoId))
+  }
+
+  return (
+    <section>
+      {newTodo.map((newItem) => (
+        <Todo key={newItem.id}>
+          <h2>{newItem.name}</h2>
+          <label>
+            Completed:
+            <input
+              type="checkbox"
+              checked={newItem.completed}
+              onChange={() =>onTodoToggle(newItem.id) }
+            />
+          </label>
+          <DeleteButton>
+            <span role="img" aria-label="delete">
+              ❌
+            </span>
+          </DeleteButton>
+        </Todo>
+      ))}
+    </section>
+  )
 }
 
 export default NewTodo
--- code/src/reducers/todos.js
+++ code/src/reducers/todos.js
@@ -1,11 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit"
 // here we create a slice that consists of name, initialstate and reducer and proporties 
+
+const data = [
+  {
+      id: '12345',
+      name: 'Charmander',
+      completed: false
+  },
+  {
+      id: '54321',
+      name: 'Bulbasaur',
+      completed: false
+  }
+]
+
 const todos = createSlice({
   name: "todos",
   initialState: {
-    items: [],
+    items: data
   },
-  reducers: {},
+  reducers: {
+    toggleItem: (store, action) => {
+      console.log(store)
+      console.log(action)
+      store.items.forEach((item) =>{
+        if (item.id === action.payload) {
+          item.completed = !item.completed
+        }
+      })
+    }
+  },
 })
 
 export default todos
