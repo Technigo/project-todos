c3bde28e32839c221bfe73a48e92051163677f26
toal13 <tomoyo.tomoyo.m@gmail.com>
2022-04-16 21:35:25 +0200

    updated app.js

--- code/src/App.js
+++ code/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-import todoList from 'reducers/todos';
-
 import Header from 'components/Header'
 import AddTodo from 'components/AddTodo';
 import TodoList from 'components/TodoList';
