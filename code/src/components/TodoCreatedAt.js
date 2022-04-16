e9d58ebbee79c1a3609b5df0693475d42c50ead8
joannaringqvist <joanna.ringqvist@gmail.com>
2022-04-16 11:35:06 +0200

    Fixed a warning

--- code/src/components/TodoCreatedAt.js
+++ code/src/components/TodoCreatedAt.js
@@ -2,14 +2,14 @@ import React from "react";
 import styled from 'styled-components';
 import { format } from 'date-fns';
 
-const createdAtText = styled.p `
+const CreatedAtText = styled.p `
     margin: 0;
     padding: 0;
 `
 
 const TodoCreatedAt = ({ createdAt }) => {
     return (
-        <createdAtText>Added {format(createdAt, 'yyyy-MM-dd hh:mm')}</createdAtText>
+        <CreatedAtText>Added {format(createdAt, 'yyyy-MM-dd hh:mm')}</CreatedAtText>
     );
 }
 
