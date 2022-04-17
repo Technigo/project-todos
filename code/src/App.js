64652897d5789f4cae45d97eace535668307f71c
Amanda Tange <amandatange@hotmail.com>
2022-04-17 21:14:03 +0200

    removed broweserroutes and navigation to have every element present in main page

--- code/src/App.js
+++ code/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { combineReducers, createStore, compose } from "@reduxjs/toolkit";
 import persistState from 'redux-localstorage';
 
 import { Container, Footer } from "./styled-components";
 
 import StartPage from "components/StartPage";
-import AddTask from "components/AddTask";
 
 import tasks from "reducers/tasks";
 
@@ -23,20 +21,13 @@ export const App = () => {
 	return (
 		<Provider store={store}>
 			<Container>
-				<BrowserRouter>
-				<Routes>
-					<Route path="/" exact
-						element={<StartPage/>}
-					/>
-					<Route path="/addtask" exact
-						element={<AddTask/>}
-					/>
-					<Route path='*' element={<Navigate to="/404" replace/>}/>
-				</Routes>
-				</BrowserRouter>
+				<StartPage />
 			</Container>
 			<Footer>
-				<p>Photo by <a href="https://unsplash.com/@evieshaffer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="noopener noreferrer">Evie S.</a></p>
+				<p>Photo by 
+					<a href="https://unsplash.com/@evieshaffer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="noopener noreferrer">Evie S.
+					</a>
+				</p>
 			</Footer>
 		</Provider>
 	);
