2e898ec29497700507f0ad9b023f0ec019301eb4
Lisen Lundgren <lisolundgren@gmail.com>
2022-04-24 10:31:38 +0200

    add incompleted todos function

--- code/src/components/TaskCount.js
+++ code/src/components/TaskCount.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 const TaskCount = () => {
 	const list = useSelector((store) => store.tasks.list);
 	const finishedTodos = list.items.filter((item) => item.isComplete).length;
+	// const incompletedTodos = list.items.filter((item) => !item.isComplete).length;
 
 	return (
 		<div className='counter'>
