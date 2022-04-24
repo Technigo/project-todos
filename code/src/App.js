4e560731ffd44f7e17cd3c81f973aaac6dda419c
Lisen Lundgren <lisolundgren@gmail.com>
2022-04-24 09:45:22 +0200

    add localstorage to Redux store state

--- code/src/App.js
+++ code/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, compose, createStore } from '@reduxjs/toolkit';
+import persistState from 'redux-localstorage';
 
 import tasks from 'reducers/tasks';
-
 import TaskList from 'components/TaskList';
+
+const enhancer = compose(persistState());
 
 const reducer = combineReducers({
 	tasks: tasks.reducer,
 });
 
-const store = configureStore({
-	reducer,
-});
+const store = createStore(reducer, enhancer);
 
 export const App = () => {
 	return (
