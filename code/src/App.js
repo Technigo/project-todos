9da3c365069bd24bda320d981749fecabed20e3c
ViktorSvenssonN <viktor.tommy.svensson@gmail.com>
2022-10-31 19:05:56 +0100

    added task reducer

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { tasks } from './reducers/tasks'
+
+const reducer = combineReducers({
+  tasks: tasks.reducer
+})
+
+const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  );
+    <Provider store={store}>
+      Your components can be mounted here, inside the Provider.
+    </Provider>
+  )
 }
