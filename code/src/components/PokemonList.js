13cad2684d74ad26559020290ae3f6f6e78329d1
Julia Ostedt <julia.ostedt@gmail.com>
2022-11-01 13:06:19 +0100

    added tuesday live session code along

---
+++ code/src/components/PokemonList.js
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import pokemons from 'reducers/pokemon';
+
+const PokemonList = () => {
+  const pokemonList = useSelector((backpack) => backpack.pokemons.items)
+
+  const dispatch = useDispatch();
+
+  const onIsCaughtToggle = (id) => {
+    dispatch(pokemons.actions.toggleItem(id))
+  }
+  return (
+    <section>
+      {pokemonList.map((singlePokemon) => {
+        return (
+          <article>
+            <h2>{singlePokemon.name}</h2>
+            <label>Is this pokemon caught?
+              <input
+                type="checkbox"
+                checked={singlePokemon.isCaught}
+                onChange={() => onIsCaughtToggle(singlePokemon.id)} />
+            </label>
+            <button type="button">❌</button>
+          </article>
+        )
+      })}
+    </section>
+  )
+}
+
+export default PokemonList
+
+// onChange={() => onPokemonIsCaughtToggle(singlePokemon.id)}
