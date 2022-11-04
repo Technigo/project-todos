f1358df662146bcfc849d4a8226d8b10c75a0e67
Scarylie <saralie.bognandi@gmail.com>
2022-11-04 09:30:55 +0100

    able to remove tasks

--- code/src/components/TaskList.js
+++ code/src/components/TaskList.js
@@ -1,33 +1,52 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import tasks from 'reducers/tasks';
 
 export const TaskList = () => {
-  const todoList = useSelector((store) => store.tasks.items); // From app.js
+  // attempt to have tasks to not reload
+  // useEffect (() => {
+  //  const listFromStorage = JSON.parse(localStorage.getIten('taskList'));
+  //  if (listFromStorage) {
+  //   dispatch(tasks.actions.addItem())
+  //  }
+  // }, [])
 
   const dispatch = useDispatch();
 
-  const onTaskIsDoneToggle = (id) => {
+  const todoList = useSelector((store) => store.tasks.items); // From app.js
+
+  const handleTaskToggle = (id) => {
     dispatch(tasks.actions.toggleTask(id));
   };
+
+  const handleRemoveTask = (taskIndex) => {
+    dispatch(tasks.actions.removeTask(taskIndex));
+  };
+
+  useEffect(() => {
+    localStorage.setItem('taskList', JSON.stringify(TaskList));
+  });
   return (
     <section>
-      {todoList.map((singleTask) => {
+      {todoList.map((singleTask, index) => {
         return (
-          <article>
-            <h2>{singleTask.text} </h2>
+          <article key={singleTask.id}>
             <label>
-              Is this task done
+              {' '}
+              Done
               <input
                 type="checkbox"
                 checked={singleTask.complete}
-                onChange={() => onTaskIsDoneToggle(singleTask.id)}
+                onChange={() => handleTaskToggle(singleTask.id)}
               />
             </label>
-            <button type="button">X</button>
+            <p>{singleTask.text} </p>
+            <button type="button" onClick={() => handleRemoveTask(index)}>
+              🗑
+            </button>
           </article>
         );
       })}
