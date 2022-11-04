e99fbbad45b00666b0ad181b8b1a0e2c5af068bc
Annika Gadman <annika.gadman@gmail.com>
2022-11-04 12:05:59 -0500

    responsive fixes

--- code/src/components/styling/GlobalStyling.js
+++ code/src/components/styling/GlobalStyling.js
@@ -6,18 +6,9 @@ export const OuterWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 100%;
+    min-height: 100vh;
     background: linear-gradient(to left, rgba(0,126,159,0.5), rgba(0,126,159,1));
 
-    @media (min-width: 668px){ 
-        height: 100vh;
-    } 
-
-    @media (min-width: 1025px) {
-        height: 100%; 
-    }
-
-}
 `
 export const InnerWrapper = styled.div`
     width: 90%;
