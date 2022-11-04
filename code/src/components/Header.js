966fdd6661e9d872ab8330d6fee14ba8553f34c4
Scarylie <saralie.bognandi@gmail.com>
2022-11-04 13:05:08 +0100

    task counter added

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import moment from 'moment';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import tasks from 'reducers/tasks';
 
 export const Header = () => {
   // Import dispatches here
   const dispatch = useDispatch();
 
-  // Get all tasks
-  // Get number of tasks in list
-  // Get completed tasks
-
   // Clear all tasks
   const handleClearTasks = (taskIndex) => {
     dispatch(tasks.actions.clearTasks(taskIndex));
   };
+
+  // Count all tasks
+  const handleTaskCounter = useSelector((store) => store.tasks.items.length);
+
+  // const totalTaskInputArray = useSelector((store) => store.tasks.items);
+  // console.log(allTasks);
+
+  // // Count checked tasks
+  // const completedTasks = handleTaskCounter.filter(
+  //   (task) => task.completed === true
+  // );
 
   return (
     <div>
@@ -23,7 +30,7 @@ export const Header = () => {
         <p>{moment().format('ddd, D MMM YYYY')}</p>
       </div>
       <div>
-        <p>1/5 complete</p>
+        <p>{handleTaskCounter}</p>
         <button type="button" onClick={handleClearTasks}>
           Clear all
         </button>
