8f53cb20beef752e727cec7fbaadeee82035b27f
Elin <elin.segelov@gmail.com>
2022-11-06 09:09:59 +0100

    Add new solution for completed counter

--- code/src/components/CompletedCounter.js
+++ code/src/components/CompletedCounter.js
@@ -1,16 +1,24 @@
+/* eslint-disable no-plusplus */
+
 import React from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 const CompletedCounter = () => {
   const toDoList = useSelector((store) => store.toDos.items);
-  const completedArray = toDoList.filter((task) => task.isCompleted === true)
+  let completedCounter = 0;
+
+  toDoList.forEach((task) => {
+    if (task.isCompleted) {
+      completedCounter++
+    }
+  });
 
   if (toDoList.length >= 1) {
     return (
       <Counter>
         {toDoList.every((task) => task.isCompleted) ? 'Yay! All done!'
-          : `${completedArray.length}/${toDoList.length} completed! `}
+          : `${completedCounter}/${toDoList.length} completed! `}
       </Counter>
     )
   }
