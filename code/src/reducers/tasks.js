8449090391fbef8a54875abfb598d3da51f2235e
Kristin Larsson <kristinlarsson@Kristins-MacBook-Air.local>
2022-11-06 21:06:48 +0100

    update uniqid

--- code/src/reducers/tasks.js
+++ code/src/reducers/tasks.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import moment from 'moment';
+import uniqid from 'uniqid'
 
 const tasks = createSlice({
   name: 'todos',
@@ -12,7 +13,8 @@ const tasks = createSlice({
     addTodo: (store, action) => {
       console.log(action);
       const newTask = {
-        id: Math.max(...store.items.map((item) => item.id)) + 1,
+        id: uniqid(),
+        // id: Math.max(...store.items.map((item) => item.id)) + 1,
         text: action.payload,
         complete: false,
         time: moment().format('D MMM HH:mm')
@@ -37,24 +39,3 @@ const tasks = createSlice({
 });
 
 export default tasks;
-
-// const exampleToSortArray = [
-//   { id: 5, text: 'Kristin', complete: false, time: moment().format('D MMM HH:MM') },
-//   { id: 2, text: 'Anders', complete: false, time: moment().format('D MMM HH:MM') },
-//   { id: 9, text: 'CArl', complete: false, time: moment().format('D MMM HH:MM') }
-// ]
-
-// exampleToSortArray.sort((a, b) => {
-//   // sort ascending (from smallest to highest)
-//   return (a.time - b.time)
-// })
-
-// console.log(exampleToSortArray)
-
-// deleteItem: (store, action) => { // access store & action
-//   store.items.splice(action.payload, 1) // starting with index 1
-//   const updatedItems = store.items.filter((item) => {
-//     return store.items.indexOf(item) !== action.payload
-//   })
-//   store.items = updatedItems
-// },
