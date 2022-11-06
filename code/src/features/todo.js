755d8c3621de6cbf0af36026f8896250cfa05b80
jessnmo <jessicamo92@gmail.com>
2022-11-06 22:03:40 +0100

    todoReducer

--- code/src/features/todo.js
+++ code/src/features/todo.js
@@ -3,8 +3,26 @@ import { createSlice } from '@reduxjs/toolkit'; //create reducers in an easy way
 const todoSlice = createSlice({
 	//contain all info about reducers, actions we want to take on the state and the name of the state
 	name: 'todo',
-	initialState: { value: {} }, //initial value exist, in value should contain the actual value of the state
+	initialState: { value: [] }, //initial value exist, in value should contain the actual value of the state
 	reducers: {
 		/* creating a function */
+		toggleToDo: (state, action) => {
+			store.value.forEach((item) => {
+				if (item.id === action.payload) {
+					item.isCompleted = !item.isCompleted;
+				}
+			});
+		},
+		addToDo: (state, action) => {
+			state.value.push(action.payload);
+		},
+		removeToDo: (state, action) => {
+			state.value.splice(action.payload, 1);
+		},
+		removeAll: (state) => {
+			state.value = [];
+		},
 	},
 });
+
+export default todoSlice.reducer;
--- code/src/index.js
+++ code/src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from './features/todo';
 
 const store = configureStore({
-	reducer: {},
+	reducer: {
+		todo: todoReducer,
+	},
 });
 const container = document.getElementById('root');
 const root = createRoot(container);
