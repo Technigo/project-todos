8a8265ea0ed314ec3d70e23e4863236817c1732d
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-04-17 16:29:02 +0200

    set up react starter

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,18 @@
+/* Import CombineReducers and configureStore */
 import React from 'react';
+import { Provider } from 'react-redux';
+
+/* const reducer = combineReducers({
+  undone: 'insert undone.reducer',
+  done: 'insert done.reducer'
+})
+
+const store = configureStore({ reducer }); */
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}>
+      <p>Todo-app comming soon!</p>
+    </Provider>
   );
 }
