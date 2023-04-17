177d7e05fad877e49a0d2911885a113d3b94448c
Frid Matilda <matilda.x.frid@scania.com>
2023-04-17 23:54:31 +0200

    added netlify link and set up for reducers

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+// import { Provider } from '@reduxjs/toolkit'
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import { xx } from 'reducers/xx';
+// import { yy } from 'reducers/yy';
+
+/*
+const reducer = combineReducers({
+  xx: xx.reducer,
+  yy: yy.reducer
+});
+
+const store = configureStore({ reducer });
+
+export const App = () => {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        Matildas todos!
+      </div>
+    </Provider>
+  );
+  */
 
 export const App = () => {
   return (
     <div className="App">
-      Matildas todos!
+          Matildas todos!
     </div>
   );
 }
