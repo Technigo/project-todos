a1b13e83645e08771324d3c702e68da6153688dd
JosefinRobertsson <josefinrobertsson@hotmail.com>
2023-04-19 00:30:31 +0200

    re-commiting file with new name

---
+++ code/src/components/TODOList.js
@@ -0,0 +1,35 @@
+/* eslint-disable react/jsx-closing-tag-location */
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import HandleTODO from './HandleTODO';
+
+// eslint-disable-next-line max-len
+// what we call addedTodos here is the store, we can reference it however we want. So we go to the store, which has a slice named tickets, and we look at the property "items". Whenever the items-array is changed, the pokemonList get's notified thanks to the useSelector, and gets updated
+const TODOList = () => {
+  const [selectedTODO, setSelectedTODO] = useState(null); // mark if a todo is clicked
+  const todoList = useSelector((addedTodos) => addedTodos.tickets.items)
+
+  const handleTODOClick = (item) => {
+    if (item === selectedTODO) {
+      setSelectedTODO(null); // deselect if clicked again
+    } else {
+      setSelectedTODO(item); // when one of the todo's is clicked, HandleTODO is mounted
+    }
+  };
+
+  return (
+    <section>
+      {todoList.map((singleTodo) => {
+        const isSelected = singleTodo === selectedTODO;
+        return <div key={singleTodo.id}>
+          <button type="button" onClick={() => handleTODOClick(singleTodo)}>
+            {singleTodo.name}
+          </button>
+          {isSelected && <HandleTODO item={singleTodo} />}
+        </div>
+      })}
+    </section>
+  )
+}
+
+export default TODOList;
