ff3d31484fe559f88f16fae1fe3e3107f2498588
Frid Matilda <matilda.x.frid@scania.com>
2023-04-18 21:30:00 +0200

    styling

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+const StyledHeadline = styled.h1`
+  font-family: 'Abril Fatface', cursive;
+  line-height: 120%;
+  color: white;
+  font-size: 72px;
+  margin-bottom: 2px;
+`;
+
+const StyledP = styled.p`
+  font-family: 'Montserrat';
+  line-height: 120%;
+  color: white;
+  font-size: 18px;
+`;
 
 const Header = () => {
   const todoList = useSelector((store) => store.tasks.items);
   console.log('todoList', todoList)
   console.log('todoList.length', todoList.length)
   return (
-    <div className="Header">
-      <h1>Matildas todos!</h1>
-      <p>{todoList.length} in the list currently</p>
-      <p>x tasks are still not done</p>
-    </div>
+    <>
+      <div className="bgtest" />
+      <div className="Header">
+        <StyledHeadline>the todos.</StyledHeadline>
+        <StyledP>{todoList.length} in the list currently</StyledP>
+        <StyledP>x tasks are still not done</StyledP>
+      </div>
+    </>
   );
 }
 
