1a4eb38f327e51824085ad955e4155dc1b141b80
Ed Code <edvard.ekstrom@gmail.com>
2023-04-18 21:40:50 +0200

    t

--- code/src/reducers/tasks.js
+++ code/src/reducers/tasks.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const tasks = createSlice({ name: 'tasks',
   initialState: [
-    { id: 1, text: 'Vejpa', complete: true },
+    { id: 1, text: 'Plugga', complete: true },
     { id: 2, text: 'Gamea', complete: false },
     { id: 3, text: 'Kodea', complete: true },
     { id: 4, text: 'Chilla', complete: true }
