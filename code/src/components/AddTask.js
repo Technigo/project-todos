00a05b2cf2e6774e23af422cc7232874307d65cf
Annaselvine <120709649+AnnaElvine@users.noreply.github.com>
2023-04-19 11:51:43 +0200

    task list works

---
+++ code/src/components/AddTask.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import tasks from 'reducers/tasks';
+
+const AddTask = () => {
+  const [inputValue, setInputValue] = useState(''); // useState använder vi för att det ska uppdateras
+  const dispatch = useDispatch();
+  const onFormSubmit = (event) => {
+    // för att sidan inte ska reload efter input event.PreventDefault måste in
+    event.preventDefault();
+    const newTask = {
+      id: Date.now().toString(),
+      name: inputValue.toUpperCase(),
+      isCaught: false
+    };
+    dispatch(tasks.actions.addTask(newTask));
+    setInputValue('');
+    // https://www.random.org/
+  }
+  return (
+    <section>
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="addTaskInput">
+                    To-Do To-Day?
+          <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} id="addTaskInput" type="text" />
+        </label>
+        <button type="submit"> Add Task!</button>
+      </form>
+    </section>
+  )
+}
+
+export default AddTask;
---
+++ code/src/reducers/tasks.js
@@ -0,0 +1,50 @@
+/* eslint-disable max-len */
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  items: [
+    {
+      id: '19asdfiuhas0823hjkadsh809',
+      name: 'Jigglypuff',
+      isCaught: false
+    },
+    {
+      id: '19asdfiuhas0823hjkadsh809',
+      name: 'Snorlax',
+      isCaught: true
+    },
+    {
+      id: '19asdfiuhas0823hjkadsh809',
+      name: 'Mewtwo',
+      isCaught: false
+    },
+    {
+      id: '19asdfiuhas0823hjkadsh809',
+      name: 'Oddish',
+      isCaught: true
+    }
+  ]
+}
+
+const tasks = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    // "store, action" can be potato, banana, first argument (store) references the state of the store, second argument (action)references the data that is
+    // passed down from a component
+    addTask: (store, action) => {
+      // Mutable way. (varje gång man muterar the state of the store, youll get a warning). MEN detta vill vi inte. Vi vill att det ska vara immutable:
+      /*    store.items.push(action.payload); */
+
+      // Immutable way:
+      store.items = [...store.items, action.payload]
+      // const x = ["a","b","c"]
+      // const y = [x, "d"] => [["a","b","c"], "d"]
+      // const z = [...x, "d"] => ["a","b","c","d"]
+      // const superObject = {a: "test", b:"other test"}
+      // const supererObject = {...superObject, c: "third test"}
+    }
+  }
+})
+
+export default tasks;
