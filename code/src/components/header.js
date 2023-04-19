253f809c6d57028f96174810650c9a1d6672634c
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-19 08:07:49 +0200

    styiling

--- code/src/components/header.js
+++ code/src/components/header.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const Title = styled.div`
 font-size: 32px;
 font-weight: bold;
-color: #333;
+color:  rgb(121, 182, 193);
 text-align: center;
 margin: 40px;
 `;
