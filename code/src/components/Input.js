01ff97718f6553d432f00cd735d31f1c06335820
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-04-19 23:37:21 +0200

    added input component

--- code/src/components/Input.js
+++ code/src/components/Input.js
@@ -1,7 +1,43 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import tasks from 'reducers/tasks';
 
+// Add id npm package for new tasks id
 const Input = () => {
-  return <div>Input</div>;
+  const [inputValue, setInputValue] = useState('');
+  const dispatch = useDispatch();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newTask = {
+      id: Date.now().toString(),
+      text: inputValue,
+      complete: false
+    };
+    dispatch(tasks.actions.addTask(newTask));
+    setInputValue('');
+  };
+  return (
+    <div className="form-container">
+      <form onSubmit={submitHandler}>
+        <label htmlFor="addTask">
+          Add new task
+          <input
+            type="text"
+            value={inputValue}
+            id="addTask"
+            onChange={(event) => {
+              setInputValue(event.target.value);
+            }}
+          />
+        </label>
+        <button type="submit">+</button>
+        <button type="button">Hide input</button>
+      </form>
+    </div>
+  );
 };
 
 export default Input;
