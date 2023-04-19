aed7d033863f91ece92b9af3c5b6d0b2b2621eb0
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-04-19 23:38:02 +0200

    added ListItems component

--- code/src/components/ListItem.js
+++ code/src/components/ListItem.js
@@ -1,11 +1,19 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import tasks from 'reducers/tasks';
 
-const ItemList = (props) => {
+const ItemList = ({ text }) => {
+  const dispatch = useDispatch();
+
+  const deleteTaskHandler = () => {};
   return (
     <div className="list-item">
       <input type="radio" />
-      <p>{props.text}</p>
-      <button type="button">To remove</button>
+      <p>{text}</p>
+      <button onClick={deleteTaskHandler()} type="button">
+        To remove
+      </button>
     </div>
   );
 };
