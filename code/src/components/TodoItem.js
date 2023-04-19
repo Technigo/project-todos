da80e866e5f257ecddd14b2998b34257d9ab9d82
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-19 15:37:18 +0200

    styling soon done

--- code/src/components/TodoItem.js
+++ code/src/components/TodoItem.js
@@ -1,38 +1,28 @@
-import React, { useRef, useState } from 'react';
-import bin from './images/trash-can-regular.svg'
-import './css/todoItem.css'
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
+import bin from './images/trash-can-regular.svg';
+import done from './images/done.png';
+import './css/todoItem.css';
 
 const TodoItem = ({ todo, index, onDelete, onDone }) => {
-  const todoRef = useRef(null);
-  const [isChecked, setIsChecked] = useState(false)
+  const [isDone, setIsDone] = useState(false);
 
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter' || event.key === ' ') {
-      event.preventDefault();
-      setIsChecked(!isChecked)
-      onDone(todo.id);
-    }
-  };
-
-  const handleCheckboxClick = () => {
-    setIsChecked(!isChecked);
+  const handleDoneButtonClick = () => {
+    setIsDone(!isDone);
     onDone(todo.id);
-  }
+  };
 
   return (
     <div className="todo-container">
-
       <p className="TodoText">{todo.text}</p>
       <section className="todo-child">
-        <label className="checkbox-container" htmlFor={todo.id}>
-          <input
-            type="checkbox"
-            ref={todoRef}
-            className="doneBtn"
-            checked={isChecked}
-            onChange={handleCheckboxClick}
-            onKeyDown={handleKeyDown} />
-        </label>
+        <button
+          className={isDone ? 'doneBtn done' : 'doneBtn'}
+          onClick={handleDoneButtonClick}
+          type="button"
+          title="Mark as done">
+          <img src={done} alt="done" />
+        </button>
         <button
           className="deleteBtn"
           onClick={() => onDelete(index)}
