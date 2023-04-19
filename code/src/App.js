fb30ad6b71c3c29f11807472d934c3883e15eff3
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-04-19 23:36:38 +0200

    fix of readability

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
 import Tasklist from 'components/Tasklist';
 import Hero from 'components/Hero';
 import Input from 'components/Input';
-
-// Redux imports
-import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { tasks } from './reducers/tasks';
+import tasks from './reducers/tasks';
 
 const reducer = combineReducers({
   tasks: tasks.reducer
