3e441778fb115a1bdba2b93047d3a9904052f11d
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-19 09:36:09 +0200

    confetti

--- code/src/components/toDoList.js
+++ code/src/components/toDoList.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { tasks } from 'reducers/todos';
-// import { NewToDo } from './newToDo';
+import JSConfetti from 'js-confetti'
 import TodoItem from './TodoItem';
 
 export const ToDoLists = () => {
@@ -9,6 +9,9 @@ export const ToDoLists = () => {
 
   // gets the tasks fron the store
   const taskList = useSelector((store) => store.tasks.items);
+
+  // confetti
+  const [showConfetti, setShowConfetti] = useState(false)
 
   // fuction to set checkbox as complete or not, from todo reducer
   const onDone = (id) => {
@@ -22,12 +25,30 @@ export const ToDoLists = () => {
   const todosTodo = taskList.filter((todo) => !todo.isDone)
   const doneTodos = taskList.filter((todo) => todo.isDone)
 
+  useEffect(() => {
+    if (todosTodo.length === 0) {
+      setShowConfetti(true)
+    } else {
+      setShowConfetti(false)
+    }
+  }, [todosTodo.length]);
+
+  useEffect(() => {
+    if (showConfetti) {
+      const JsConfetti = new JSConfetti();
+      JsConfetti.addConfetti();
+    }
+  }, [showConfetti]);
+
   return (
     <>
       <h2>To-do ({todosTodo.length})</h2>
 
       {todosTodo.length === 0 && (
-        <p>You are all DONE! *high five*</p>
+        <>
+          <p>You are all done! *high five*</p>
+          {showConfetti && <JSConfetti />}
+        </>
       )}
 
       {todosTodo.map((todo, index) => (<TodoItem
