42850d3a732d0b3bc5b41e108a69ba5b72dea999
SandraMadeleine <sandramohlsson@yahoo.com>
2023-04-19 22:29:30 +0200

    working on the list

--- code/src/components/ToDo.js
+++ code/src/components/ToDo.js
@@ -1,22 +1,40 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 import tasks from 'reducers/list'
 
 const ToDo = () => {
-  const todoList = useSelector((store) => store.todos.items)
+  const todoList = useSelector((store) => store.tasks.items)
 
   const dispatch = useDispatch()
 
-  const onToggleTodo = (todoId) => {
-    dispatch(todos.actions.toggleItem(todoId))
+  const ToggleTodo = (todoId) => {
+    dispatch(tasks.actions.toggleItem(todoId))
   }
 
-  const onDeleteTodo = (index) => {
-    dispatch(todos.actions.deleteItem(index))
+  const DeleteTodo = (index) => {
+    dispatch(tasks.actions.deleteItem(index))
+  }
+
+  return (
+    todoList.map((todoItem, todoIndex) => (
+      <TodoItem key={todoItem.id}>
+        <TodoItemText>{todoItem.name}</TodoItemText>
+        <input
+          className="checkbox"
+          type="checkbox"
+          checked={todoItem.isDone}
+          onChange={() => ToggleTodo(todoItem.id)} />
+        <label htmlFor="done">CHECK!</label>
+        <DelButton onClick={() => DeleteTodo(todoIndex)}>
+          <Delete role="img" aria-label="delete">
+            {/* ✖  */}
+             ❎
+          </Delete>
+        </DelButton>
+      </TodoItem>
+    ))
+  )
 }
 
-return (
-
-)
-}
+export default ToDo;
