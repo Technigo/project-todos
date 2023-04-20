9715f633e55829303abf737ec5361c0606c7a6d4
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-20 15:28:19 +0200

    changed with on the delete and done button

--- code/src/components/TodoItem.js
+++ code/src/components/TodoItem.js
@@ -1,4 +1,4 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
+
 import React, { useState } from 'react';
 import bin from './images/trash-can-regular.svg';
 import done from './images/done.png';
