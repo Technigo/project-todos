0f70fc863e494256a4bd3fa5f7a3eda32f10ae3d
Annaselvine <120709649+AnnaElvine@users.noreply.github.com>
2023-04-20 21:04:49 +0200

    delete buttons fixed

--- code/src/components/AddTask.js
+++ code/src/components/AddTask.js
@@ -18,16 +18,18 @@ const AddTask = () => {
     // https://www.random.org/
   }
   return (
-    <section>
-      <p>Plans for today</p>
-      <form onSubmit={onFormSubmit}>
-        <label htmlFor="addTaskInput">
-          <button type="submit"> Add</button>
-          {/* Here user writes the dotos - input */}
-          <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} id="addTaskInput" type="text" />
-        </label>
-      </form>
-    </section>
+    <div className="container">
+      <section>
+        <p>Plans for today</p>
+        <form onSubmit={onFormSubmit}>
+          <label htmlFor="addTaskInput">
+            <button type="submit"> Add</button>
+            {/* Here user writes the dotos - input */}
+            <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} id="addTaskInput" type="text" />
+          </label>
+        </form>
+      </section>
+    </div>
   )
 }
 
