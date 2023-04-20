9d457d8c1e697676cd3292ac648cc2c67b722794
Annaselvine <120709649+AnnaElvine@users.noreply.github.com>
2023-04-20 14:31:30 +0200

    github link added

--- code/src/App.js
+++ code/src/App.js
@@ -4,7 +4,7 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import AddTask from 'components/AddTask';
 import TaskList from 'components/TaskList';
 import tasks from 'reducers/tasks';
-import { Footer } from 'components/Footer';
+import { Footer } from 'components/footer';
 
 export const App = () => {
   const reducer = combineReducers({
--- code/src/components/footer.js
+++ code/src/components/footer.js
@@ -1,26 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
-import Githubicon from './Images/githubicon.png'
+import Github from '../icons/githubicon.png'
 
 const FooterWrapper = styled.footer`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  gap: 30px;
-  opacity: 30%;
-  position: absolute;
-  bottom: -100px;
-`;
-const Name = styled.p`
-  font-family: 'Archivo Black', sans-serif;
-  font-size: 11px;
-  color: black;
-  opacity: 50%;
+justify-content: center;
+opacity: 0.9;
+bottom: 150px;
 `;
 const GitLink = styled.img`
-  width: 30px;
+  width: 34px;
   &:hover {
     color: white;
     filter: brightness(100); 
@@ -31,17 +19,18 @@ const StyledA = styled.a`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  padding-top: 146px;
+
 `;
 
 export const Footer = () => {
   return (
-
     <FooterWrapper>
       <StyledA href="https://github.com/AnnaElvine">
-        <Name>Anna</Name>
-        <GitLink src={Githubicon} alt="github" />
+        <GitLink src={Github} alt="github" />
       </StyledA>
     </FooterWrapper>
   );
 };
+
+export default Footer;
