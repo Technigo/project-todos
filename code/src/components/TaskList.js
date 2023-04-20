f8d2e4de3e618f7c1fa9e1581311b6dff73f8a95
Ed Code <edvard.ekstrom@gmail.com>
2023-04-20 22:19:28 +0200

    hamburger menu

--- code/src/components/TaskList.js
+++ code/src/components/TaskList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/macro'
 import { useSelector, useDispatch } from 'react-redux'
 import { tasks } from '../reducers/tasks'
@@ -11,9 +11,15 @@ gap: 5px;
 width: 100%;`
 
 const StyledTasklistBar = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+padding: 5px;
+align-items: center;
 background-color: var(--foreground-primary-color);
 
 h1 {
+  flex: 1;
   color: white;
   text-align: center;
 }
@@ -21,22 +27,55 @@ h1 {
 `
 
 const StyledActionBar = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: center;
-gap: 10px;`
+display: none;
+height: 0px;
+opacity: 0;
+transition: opacity 1s ease-out;
+overflow: hidden;
+
+
+&.active {
+  opacity: 1;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 10px;
+  padding: 2px;
+}
+`
+
+const HamburgerMenuButton = styled.button`
+width: 30px;
+height: 30px;
+background-color: inherit;
+border: none;
+
+::before {
+  content: "=";
+}
+`
 
 const CompleteAllButton = styled.button`
-background-color: green;
-width: 15px;
-height: 15px;`
+font-size: 12px;
+font-weight: 400;
+background-color: transparent;
+border: 1px solid grey;
+border-radius: 3px;
+width: 50%;
+`
 
-const DeleteAllButton = styled.button`
-background-color: red;
-width: 15px;
-height: 15px;`
+const DeleteAllButton = styled(CompleteAllButton)`
+color: red;
+`
 
 const TaskList = () => {
+  const [toggledActionBar, setToggledActionBar] = useState(true);
+
+  const toggleActionBar = () => {
+    setToggledActionBar(!toggledActionBar);
+  }
+
   const dispatch = useDispatch();
 
   const handleCompleteAll = () => {
@@ -51,12 +90,12 @@ const TaskList = () => {
   return (
     <StyledContainer>
       <StyledTasklistBar>
-
+        <HamburgerMenuButton onClick={toggleActionBar} />
         <h1>Tasklist</h1>
       </StyledTasklistBar>
-      <StyledActionBar>
-        <CompleteAllButton value="Complete All" onClick={handleCompleteAll} />
-        <DeleteAllButton value="Delete All" onClick={handleDeleteAll} />
+      <StyledActionBar className={toggledActionBar && 'active'}>
+        <CompleteAllButton value="Complete All" onClick={handleCompleteAll}>complete all</CompleteAllButton>
+        <DeleteAllButton value="Delete All" onClick={handleDeleteAll}>delete all</DeleteAllButton>
       </StyledActionBar>
       {tasksData.map((task) => (
         <Task id={task.id} key={task.id} />))}
