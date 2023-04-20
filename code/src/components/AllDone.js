b6017e989af35b1c6f3ee77bdb6002e16630e2b6
Emilia Saberski <emilia@saberski.com>
2023-04-20 16:10:00 +0200

    Changed confetti to show on all done instead

--- code/src/components/AllDone.js
+++ code/src/components/AllDone.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useWindowSize } from '@react-hook/window-size';
-import Confetti from 'react-confetti'
 // import styled from 'styled-components';
 
 export const AllDone = () => {
   const items = useSelector((store) => store.tasks.items)
-  const { width, height } = useWindowSize()
 
   const allDone = () => {
     if (items.length === 0) {
-      return (
-        <>
-          <Confetti
-            width={width}
-            height={height}
-            colors={['#000', '#d4af37']}
-            numberOfPieces={100}
-            confettiSource={{ x: 0, y: 500, w: 1000, h: 1000 }} />
-          <h2>All done </h2>
-        </>
-      )
+      return (<h2>Nothing here yet</h2>)
     }
   }
   return (
--- code/src/components/Counter.js
+++ code/src/components/Counter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useWindowSize } from '@react-hook/window-size';
+import Confetti from 'react-confetti'
 import styled from 'styled-components';
 
 const CounterText = styled.p`
@@ -11,12 +13,23 @@ const CounterText = styled.p`
 export const Counter = () => {
   const items = useSelector((store) => store.tasks.items)
   const completedTasks = items.filter((task) => task.complete);
+  const { width, height } = useWindowSize()
 
   const doneTasks = () => {
     if (items.length === 0) {
       return 'Add task'
     } else if (items.length === completedTasks.length) {
-      return 'All done'
+      return (
+        <>
+          <Confetti
+            width={width}
+            height={height}
+            colors={['#000', '#d4af37']}
+            numberOfPieces={100}
+            gravity={0.05}
+            confettiSource={{ x: 0, y: 500, w: 1000, h: 1000 }} />
+          <p>All done</p>
+        </>)
     } else {
       return `${completedTasks.length} / ${items.length} done`
     }
