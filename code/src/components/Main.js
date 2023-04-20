50d0d3bc92ed765d10049bfcfe85af3bac4beb95
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-04-20 11:06:52 +0200

    Change import

--- code/src/components/Main.js
+++ code/src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import tasks from 'reducers/task';
-import { TaskList } from './TaskList';
+import TaskList from './TaskList';
 
 const reducer = combineReducers({
   tasks: tasks.reducer
