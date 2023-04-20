5bed866c2c07b9f7af58ffaca2401c8c6fe63945
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-20 08:47:32 +0200

    styling

--- code/src/components/footer.js
+++ code/src/components/footer.js
@@ -4,7 +4,7 @@ import linkedin from './images/blacklinkedinicon.svg'
 export const Footer = () => {
   return (
     <footer className="Footer">
-      <p>Tjudili n A website made by Movimiento © 2023 sudent @ Technigo </p>
+      <p>A website made by Movimiento © 2023 sudent @ Technigo </p>
       <div className="iconslinkedin">
         <a href="https://www.linkedin.com/in/irup%C3%A9-pozo-graviz-119112266/">
           <img src={linkedin} alt="button to linkedin black" />
