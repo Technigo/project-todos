c2cb3681a891bd010e7393ebad711e7850aa3c98
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-20 15:48:16 +0200

    fixed a typo

--- code/src/components/footer.js
+++ code/src/components/footer.js
@@ -4,7 +4,7 @@ import linkedin from './images/blacklinkedinicon.svg'
 export const Footer = () => {
   return (
     <footer className="Footer">
-      <p>A website made by Movimiento © 2023 sudent @ Technigo </p>
+      <p>A website made by Movimiento © 2023 student @ Technigo </p>
       <div className="iconslinkedin">
         <a href="https://www.linkedin.com/in/irup%C3%A9-pozo-graviz-119112266/">
           <img src={linkedin} alt="button to linkedin black" />
