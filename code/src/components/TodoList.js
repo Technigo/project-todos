e8c4fafb2fd20d6633684ecdd32cfe0166ed8f39
Frid Matilda <matilda.x.frid@scania.com>
2023-04-20 19:37:15 +0200

    empty page

--- code/src/components/TodoList.js
+++ code/src/components/TodoList.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { DeleteButton } from './styled_components/buttons';
 import { tasks } from '../reducers/tasks';
+import Empty from './Empty'
 
 const colors = ['#FBF8CC', '#FDE4CF', '#FFCFD2', '#F1C0E8', '#CFBAF0', '#A3C4F3', '#90DBF4', '#8EECF5', '#98F5E1', '#B9FBC0'];
 
@@ -36,22 +37,27 @@ const TodoList = ({ selectedTab }) => {
   // Här vill jag få in en empty page som visas när den filtrerade arrayen är tom.
   // Conditional filteredList.length === 0
   return (
-    <div className="List">
-      <ul>
-        {filteredList.map((singleTodo, index) => {
-          return (
-            <StyledTodoRow key={singleTodo.id} index={index}>
-              <label htmlFor="todo" className="todo-label">
-                <span>{singleTodo.content}</span>
-                <input type="checkbox" id={singleTodo.id} name="todos" onChange={() => dispatch(tasks.actions.handleCheck(singleTodo))} />
-              </label>
-              <DeleteButton type="button" onClick={() => dispatch(tasks.actions.removeItem(singleTodo))}>Delete</DeleteButton>
-            </StyledTodoRow>
-          )
-        })}
-        <DeleteButton type="button" onClick={() => dispatch(tasks.actions.removeAll())}>Remove all tasks</DeleteButton>
-      </ul>
-    </div>
+    <>
+      {filteredList.length > 0
+    && (
+      <div className="List">
+        <ul>
+          {filteredList.map((singleTodo, index) => {
+            return (
+              <StyledTodoRow key={singleTodo.id} index={index}>
+                <label htmlFor="todo" className="todo-label">
+                  <span>{singleTodo.content}</span>
+                  <input type="checkbox" id={singleTodo.id} name="todos" onChange={() => dispatch(tasks.actions.handleCheck(singleTodo))} />
+                </label>
+                <DeleteButton type="button" onClick={() => dispatch(tasks.actions.removeItem(singleTodo))}>Delete</DeleteButton>
+              </StyledTodoRow>
+            )
+          })}
+          <DeleteButton type="button" onClick={() => dispatch(tasks.actions.removeAll())}>Remove all tasks</DeleteButton>
+        </ul>
+      </div>)}
+      {filteredList.length === 0 && <Empty selectedTab={selectedTab} />}
+    </>
   );
 }
 
