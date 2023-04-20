0287c7ba16e38e8455b15dae07e6c3452c96410c
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-04-20 13:54:01 +0200

    Change from px to rem

--- code/src/components/Global.js
+++ code/src/components/Global.js
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  width: 6.25rem;
+  width: 8rem;
   padding: 0.5rem;
   font-size: 0.9375rem;
   border-radius: 0.25rem;
+  border: none;
   transition-duration: 0.4s;
-  box-shadow: none;
+  box-shadow: var(--box-shadow);
   margin: 0.625rem;
   font-family: var(--paragraph-font-family);
+  cursor: pointer;
 
   &:hover {
     background-color: white;
@@ -19,19 +21,24 @@ export const Button = styled.button`
 `;
 
 export const AddButton = styled(Button)`
-  border: 0.0625rem solid black;
   background-color:var(--btn1-color);
   color: black;
 `;
 
 export const DeleteButton = styled(Button)`
-  border: 0.0625rem solid black;
   background-color: #f25050;
   color: black;
 `;
 
 export const DeleteAllButton = styled(Button)`
-  border: 0.0625rem solid black;
   background-color: #f25050;
   color: black;
+`;
+
+export const Lists = styled.ul`
+font-family: var(--paragraph-font-family);
+`;
+
+export const StyledTaskList = styled(Lists)`
+list-style: none;
 `;
