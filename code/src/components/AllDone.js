271a6ea9b7f78413a289093b4748c4f27805fd90
Emilia Saberski <emilia@saberski.com>
2023-04-20 13:26:24 +0200

    Added empty state page

--- code/src/components/AllDone.js
+++ code/src/components/AllDone.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useWindowSize } from '@react-hook/window-size';
+import Confetti from 'react-confetti'
+// import styled from 'styled-components';
 
 export const AllDone = () => {
-  const [emptyState, setEmptyState] = useState([])
+  const items = useSelector((store) => store.tasks.items)
+  const { width, height } = useWindowSize()
 
-  const EmptyState = () => {
-    if (emptyState === 0) {
-      return 'All done'
+  const allDone = () => {
+    if (items.length === 0) {
+      return (
+        <>
+          <Confetti
+            width={width}
+            height={height}
+            colors={['#000', '#d4af37']}
+            numberOfPieces={100}
+            confettiSource={{ x: 0, y: 500, w: 1000, h: 1000 }} />
+          <h2>All done </h2>
+        </>
+      )
     }
   }
-  return <p>{EmptyState}</p>
+  return (
+    <span>
+      {allDone()}
+    </span>
+  )
 }
