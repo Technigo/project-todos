6eb8872bc2a7a72d77e7863ac22ab3a5d8bfc1d3
JosefinRobertsson <josefinrobertsson@hotmail.com>
2023-04-22 14:49:53 +0200

    +1

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,24 +1,28 @@
-/* eslint-disable max-len */
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const Header = () => {
   const todoList = useSelector((addedTodos) => addedTodos.tickets.items)
-  console.log(todoList)
-  const [todoCount, setTodoCount] = useState(Math.max(todoList.length - 1, 0)); // Math.max() returns the highest number, so we can use it to not display a negative number when there are no user-added items. Without subratcting 1, the counter would jump from 0 to 2 when the first user item is added.
+  const [todoCount, setTodoCount] = useState(0);
   const [isDoneCount, setIsDoneCount] = useState(0);
 
   useEffect(() => {
-    // checks if the one item in the todolist is the intial one with the id of empty string, and if so it sets the count to 0. Otherwise the "invisible" first item is counted and the empty state has a counter that displays 0/1
+    if (todoList.length === 0) {
+      setTodoCount(0);
+      setIsDoneCount(0);
+      return;
+    }
+
     if (todoList.length === 1 && todoList[0].id === '') {
       setTodoCount(0);
       setIsDoneCount(0);
       return;
     }
-    setTodoCount(todoList.length - 1);
+
+    setTodoCount(todoList.length);
     setIsDoneCount(todoList.filter((todo) => todo.isDone).length);
   }, [todoList]);
-  console.log(isDoneCount)
+
   return (
     <>
       <h1>Todo&apos;s</h1>
