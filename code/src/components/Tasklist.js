217fd27c7f3b9e7a9d787bf43e395514176817e7
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-04-22 21:30:38 +0200

    added deleteTask function

--- code/src/components/Tasklist.js
+++ code/src/components/Tasklist.js
@@ -1,18 +1,30 @@
+/* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { tasks } from 'reducers/tasks';
-import ListItem from './ListItem';
+// import ListItem from './ListItem';
 
 const Tasklist = () => {
-  const items = useSelector((state) => state.tasks.tasks);
+  const items = useSelector((store) => store.tasks.tasks);
+  const dispatch = useDispatch();
+
   items.map((item) => console.log(item.text));
 
   return (
     <section className="task-list">
-      {items.map((item) => (
-        <ListItem key={item.id} text={item.text} />
+      {items.map((todo) => (
+        <div className="list-item">
+          <input type="radio" />
+          <p>{todo.text}</p>
+          <button
+            onClick={() => dispatch(tasks.actions.deleteTask(todo))}
+            type="button"
+          >
+            To remove
+          </button>
+        </div>
       ))}
     </section>
   );
