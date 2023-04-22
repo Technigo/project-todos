3975d5ed2b22c0fdd4a1115253e8666175c1aa9e
JosefinRobertsson <josefinrobertsson@hotmail.com>
2023-04-22 13:48:21 +0200

    fixing counter including initialState in Header

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -8,6 +8,13 @@ const Header = () => {
   const [isDoneCount, setIsDoneCount] = useState(0);
 
   useEffect(() => {
+    // eslint-disable-next-line max-len
+    // checks if the one item in the todolist is the intial one with the id of empty string, and if so it sets the counts to 0. Otherwise the "invisible" first item is counted and the empty state has a counter that displays 0/1
+    if (todoList.length === 1 && todoList[0].id === '') {
+      setTodoCount(0);
+      setIsDoneCount(0);
+      return;
+    }
     setTodoCount(todoList.length);
     setIsDoneCount(todoList.filter((todo) => todo.isDone).length);
   }, [todoList]);
