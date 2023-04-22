228ed10bcbfa173602fcab28ea1d23f14c7f65e5
Emma Engvall <emma.engvall@ica.se>
2023-04-22 22:29:01 +0200

    changed to capitalize first letter for new item

--- code/src/components/AddPokemon.js
+++ code/src/components/AddPokemon.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import pokemons from 'reducers/pokemon';
 
+Object.defineProperty(String.prototype, 'capitalize', {
+  value: function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  },
+  enumerable: false
+});
+
 const AddPokemon = () => {
   const [inputValue, setInputValue] = useState('');
   const dispatch = useDispatch();
@@ -9,7 +16,7 @@ const AddPokemon = () => {
     event.preventDefault();
     const newPokemon = {
       id: Date.now().toString(),
-      name: inputValue.toUpperCase(),
+      name: inputValue.capitalize(),
       isCaught: false
     }
     // false. so that it is not checked.
@@ -26,7 +33,7 @@ const AddPokemon = () => {
       <form onSubmit={onFormSubmit}>
         <label htmlFor="addPokemonInput">
         Add your new Pokemon here.
-          <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} id="addPokemonInput" type="text" />
+          <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} id="addPokemonInput" type="text" required />
         </label>
         <button type="submit"> Add now!</button>
       </form>
