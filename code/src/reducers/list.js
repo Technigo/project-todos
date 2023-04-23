03d2d62f09c2395fdd4f0a76da3ad50418d8cc1a
SandraMadeleine <sandramohlsson@yahoo.com>
2023-04-23 13:24:32 +0200

    re-doing the list

--- code/src/reducers/list.js
+++ code/src/reducers/list.js
@@ -1,17 +1,12 @@
 /* eslint-disable max-len */
 import { createSlice } from '@reduxjs/toolkit';
 
-// The slice for todo has the initial state with an array for each of the single todo-items
-// Every todo item has an id, text and isDone property.
+const data = []
 
-export const todos = createSlice({
+export const list = createSlice({
   name: 'todos',
   initialState: {
-    items: [
-      { id: '1', text: 'Click this to-do to mark it as done ✅', isDone: false },
-      { id: '2', text: 'Your first to-do is: Sing like no one is hearing!', isDone: false }
-    ]
-    //
+    items: data
   },
   // The reducers field contains functions to modify the state of the slice (todo made with createSlice)
   reducers: {
@@ -38,27 +33,3 @@ export const todos = createSlice({
     }
   }
 });
-// Mutable
-//   store.items.push(action.payload);
-// Immutable
-// store.items = [...store.items, action.payload];
-
-// const initialState = {
-//   items: [
-//     {
-//       id: '189438fdjhrjejioe9845',
-//       name: 'ToDo1',
-//       isNew: false
-//     },
-//     {
-//       id: '189438fdjhrjejioe9846',
-//       name: 'ToDo2',
-//       isNew: false
-//     },
-//     {
-//       id: '189438fdjhrjejioe9847',
-//       name: 'ToDo3',
-//       isNew: false
-//     }
-//   ]
-// }
