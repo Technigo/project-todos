c8c11162a8f5871d81877a0f8ed2e3cb62a3c7ac
Andrea <andrea@Andreas-Air.lan>
2023-04-23 02:50:16 +0200

    added weather-icon

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const HeaderWrapper = styled.div`
+background: beige;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+
 const HeaderText = styled.div`
 font-size: 25px;
 font-family: 'Quicksand', sans-serif;
 height: 100px;
-background: pink;
+margin-bottom: 20px;
+`
+const Weather = styled.div`
+font-size: 20px;
+font-famiy: 'Quicksand', sans-serif;
 margin-bottom: 20px;
 `
 
 export const Header = () => {
   return (
-    <HeaderText>
-      <h1>Back & forth list</h1>
-    </HeaderText>
+    <HeaderWrapper>
+      <HeaderText>
+        <h1>Back & forth...</h1>
+      </HeaderText>
+      <Weather>
+        It's <span class="material-symbols-outlined">
+rainy
+</span> today.
+      </Weather>
+    </HeaderWrapper>
   )
 }
 
