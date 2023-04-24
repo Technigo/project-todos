edb122af655e63024360c2bafd89be394b3bb874
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-04-24 08:06:43 +0200

    Trying again...

--- code/src/App.js
+++ code/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store'
 import { PersistGate } from 'redux-persist/integration/react';
-import { ToDoList } from 'components/ToDoList/ToDoList.js';
+import { ToDoList } from 'components/ToDoList/ToDoList';
 import { GlobalStyle } from 'styles/GlobalStyle';
 import { Header } from 'components/Header/Header';
 import { Footer } from 'components/Footer/Footer';
@@ -17,7 +17,5 @@ export const App = () => {
         <Footer />
       </PersistGate>
     </Provider>
-  )
-}
-
-// ctrl + c inside the terminal to stop the liveServer
+  );
+};
