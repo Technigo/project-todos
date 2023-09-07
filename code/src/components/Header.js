429d98e581558f301778c4e81be59521124feaef
augustaquilon <august.aquilon@gmail.com>
2023-09-07 13:16:01 +0200

    started reworking the design + accessibility

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,8 +1,6 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import styled from 'styled-components';
-import { Player, Controls } from '@lottiefiles/react-lottie-player';
-/* import logoImage from '../assets/To-Do List-logos-1/To-Do List-logos_white.png' */
 
 const InnerWrapper = styled.section`
 display: flex;
@@ -27,17 +25,8 @@ export const Header = () => {
   return (
     <OuterWrapper>
       <InnerWrapper>
-        <div className="lazy-cat-header">
-          <Player
-            autoplay
-            loop
-            src="https://lottie.host/e1545cc9-29b0-4fc7-bf99-cba9800c4de6/LHo0WS7dLJ.json"
-            style={{ height: '150px', width: '150px' }}>
-            <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
-          </Player>
-        </div>
+        <Title>To-Do List</Title>
       </InnerWrapper>
-      <Title>To-Do List</Title>
     </OuterWrapper>
   )
 }
